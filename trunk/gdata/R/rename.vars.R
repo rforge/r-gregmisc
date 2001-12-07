a55a9695701e8a3c59d05c0e594f6c0002454968
warneg <warneg@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2001-12-07 21:40:54 +0000

    Initial checkin
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@38 45178186-c70b-4eec-b05e-9f6c5a094bf4

---
+++ trunk/gdata/R/rename.vars.R
@@ -0,0 +1,48 @@
+# $Id$
+#
+# $Log$
+# Revision 1.1  2001/12/07 21:40:54  warneg
+# Initial checkin
+#
+#
+
+rename.vars <- function(data,from='',to='',info=T) {
+
+   dsn <- deparse(substitute(data))
+   dfn <- names(data)
+
+   if ( length(from) != length(to)) {
+     cat('--------- from and to not same length ---------\n')
+     stop()
+   }
+
+   if (length(dfn) < length(to)) {
+     cat('--------- too many new names ---------\n')
+     stop()
+   }
+
+   chng <- match(from,dfn)
+
+   frm.in <- from %in% dfn
+   if (!all(frm.in) ) {
+     cat('---------- some of the from names not found in',dsn,'\n')
+     stop()
+   }
+
+   if (length(to) != length(unique(to))) {
+     cat('---------- New names not unique\n')
+     stop()
+   }
+
+   dfn.new <- dfn
+   dfn.new[chng] <- to
+   if (info) cat('\nChanging in',dsn)
+     tmp <- rbind(from,to)
+     dimnames(tmp)[[1]] <- c('From:','To:')
+     dimnames(tmp)[[2]] <- rep('',length(from))
+   if (info) print(tmp,quote=F)
+   names(data) <- dfn.new
+   invisible(data)
+}
+
+
