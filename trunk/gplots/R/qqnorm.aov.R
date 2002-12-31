e9a7eb4a409cdef83d7e88a7470d522f9e0b61fe
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2002-12-31 19:50:59 +0000

    Initial checkin of qqnorm.aov function and documentation submitted by Kjetil Halvorsen <kjetilh@jupiter.umsanet.edu.bo>.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@145 45178186-c70b-4eec-b05e-9f6c5a094bf4

---
+++ trunk/gplots/R/qqnorm.aov.R
@@ -0,0 +1,43 @@
+# $Id$
+#
+# $Log$
+# Revision 1.1  2002/12/31 19:50:58  warnes
+# Initial checkin of qqnorm.aov function and documentation submitted by
+# Kjetil Halvorsen <kjetilh@jupiter.umsanet.edu.bo>.
+#
+#
+
+qqnorm.aov <- function (obj, full = FALSE, label = FALSE, omit = NULL,
+                        xlab = paste(if(full) "" else "Half", " Normal plot"),
+                        ylab = "Effects", ...)
+{
+    r <- obj$rank
+    eff <- if (full)
+        effects(obj, set.sign = TRUE)[1:r]
+    else abs(effects(obj))[1:r]
+    na <- names(eff)
+    int <- match("(Intercept)", na)
+    if (!is.null(omit)) {
+        if (is.character(omit)) {
+            int <- c(int, match(omit, na))
+        }
+        else int <- c(int, omit)
+    }
+    int <- int[!is.na(int)]
+    if (length(int))
+        eff <- eff[-int]
+    n <- length(eff)
+    if (n <= 0)
+        stop("Not enough effects")
+    ord <- order(eff)
+    na <- names(eff)
+    P <- if (full)
+        ppoints(n)
+    else ((1:n) + n)/(2 * n + 1)
+    Q <- qnorm(P)
+    plot(x = Q, y = eff[ord], xlab = xlab, ylab = ylab, ...)
+    if (label && dev.interactive())
+        identify(Q, eff[ord], names(eff)[ord])
+}
+
+
