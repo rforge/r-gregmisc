764e89bde0924b3a849626af266889fabc3ca007
(no author) <(no author)@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2003-01-02 16:11:44 +0000

    This commit was manufactured by cvs2svn to create tag 'rel_0_7_5'.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@152 45178186-c70b-4eec-b05e-9f6c5a094bf4

---
+++ tags/rel_0_7_5/gmodels/R/CrossTable.R
@@ -0,0 +1,182 @@
+# $Id$
+#
+# $Log$
+# Revision 1.2  2002/11/04 14:13:57  warnes
+# - Moved fisher.test() to after table is printed, so that table is
+#   still printed in the event that fisher.test() results in errors.
+#
+# Revision 1.1  2002/09/23 13:38:53  warnes
+#
+# - Added CrossTable() and barplot2() code and docs contributed by Marc Schwartz.
+# - Permit combinations() to be used when r>n provided repeat.allowed=TRUE
+# - Bumped up version number
+
+CrossTable <- function (x, y, digits = 3, expected = FALSE, correct = TRUE)
+{
+  # require(ctest)  # included in base and loaded automatically
+
+  if (missing(y))
+  {
+    # if only x is specified, it must be a 2 dimensional matrix
+    if (length(dim(x)) != 2)
+      stop("x must be a 2 dimensional matrix if y is not given")
+    if(any(dim(x) < 2))
+      stop("x must have at least 2 rows and columns")
+    if(any(x < 0) || any(is.na(x)))
+      stop("all entries of x must be nonnegative and finite")
+    else
+      t <- x
+  }
+  else
+  {
+    if(length(x) != length(y))
+      stop("x and y must have the same length")
+
+    # Create Titles for Table From Vector Names
+    RowData <- deparse(substitute(x))
+    ColData <- deparse(substitute(y))
+
+    # Remove unused factor levels from vectors
+    x <- factor(x)
+    y <- factor(y)
+
+    if((nlevels(x) < 2) || (nlevels(y) < 2))
+      stop("x and y must have at least 2 levels")
+
+    # Generate table
+    t <- table(x, y)
+  }
+
+  # Generate cell proportion of row
+  CPR <- prop.table(t, 1)
+
+  # Generate cell proportion of col
+  CPC <- prop.table(t, 2)
+
+  # Generate cell proportion of total
+  CPT <- prop.table(t)
+
+  # Generate summary counts
+  GT <- sum(t)
+  RS <- rowSums(t)
+  CS <- colSums(t)
+
+  # Column and Row Total Headings
+  ColTotal <- "Column Total"
+  RowTotal <- "Row Total"
+
+  # Set consistent column widths based upon dimnames and table values
+  CWidth <- max(digits + 2, c(nchar(t), nchar(dimnames(t)[[2]]), nchar(RS), nchar(CS), nchar(RowTotal)))
+  RWidth <- max(c(nchar(dimnames(t)[[1]]), nchar(ColTotal)))
+
+  # Adjust first column width if Data Titles present
+  if (exists("RowData"))
+    RWidth <- max(RWidth, nchar(RowData))
+
+  # Create row separators
+  RowSep <- paste(rep("-", CWidth + 2), collapse = "")
+  RowSep1 <- paste(rep("-", RWidth + 1), collapse = "")
+  SpaceSep1 <- paste(rep(" ", RWidth), collapse = "")
+  SpaceSep2 <- paste(rep(" ", CWidth), collapse = "")
+
+  # Create formatted Names
+  FirstCol <- formatC(dimnames(t)[[1]], width = RWidth, format = "s")
+  ColTotal <- formatC(ColTotal, width = RWidth, format = "s")
+  RowTotal <- formatC(RowTotal, width = CWidth, format = "s")
+
+  # Perform Chi-Square Test
+  CST <- chisq.test(t, correct = correct)
+
+
+  # Print Cell Layout
+
+  cat(rep("\n", 2))
+  cat("|-----------------|\n")
+  cat("|               N |\n")
+  if (expected)
+    cat("|      Expected N |\n")
+  cat("|   N / Row Total |\n")
+  cat("|   N / Col Total |\n")
+  cat("| N / Table Total |\n")
+  cat("|-----------------|\n")
+  cat(rep("\n", 2))
+  cat("Total Observations in Table: ", GT, "\n")
+  cat(rep("\n", 2))
+
+  # Print Column headings
+
+  if (exists("RowData"))
+  {
+    cat(SpaceSep1, "|", ColData, "\n")
+    cat(formatC(RowData, width = RWidth, format = "s"), formatC(dimnames(t)[[2]], width = CWidth, format = "s"), RowTotal, sep = " | ", collapse = "\n")
+  }
+  else
+    cat(SpaceSep1, formatC(dimnames(t)[[2]], width = CWidth, format = "s"), RowTotal, sep = " | ", collapse = "\n")
+
+  cat(RowSep1, rep(RowSep, ncol(t) + 1), sep = "|", collapse = "\n")
+
+  # Print table cells
+  for (i in 1:nrow(t))
+  {
+    cat(FirstCol[i], formatC(c(t[i, ], RS[i]), width = CWidth), sep = " | ", collapse = "\n")
+
+    if (expected)
+      cat(SpaceSep1, formatC(CST$expected[i, ], digits = digits, format = "f", width = CWidth), SpaceSep2, sep = " | ", collapse = "\n")
+
+    cat(SpaceSep1, formatC(c(CPR[i, ], RS[i] / GT), width = CWidth, digits = digits, format = "f"), sep = " | ", collapse = "\n")
+    cat(SpaceSep1, formatC(CPC[i, ], width = CWidth, digits = digits, format = "f"), SpaceSep2, sep = " | ", collapse = "\n")
+    cat(SpaceSep1, formatC(CPT[i, ], width = CWidth, digits = digits, format = "f"), SpaceSep2, sep = " | ", collapse = "\n")
+
+    cat(RowSep1, rep(RowSep, ncol(t) + 1), sep = "|", collapse = "\n")
+  }
+
+  # Print Column Totals
+  cat(ColTotal, formatC(c(CS, GT), width = CWidth), sep = " | ", collapse = "\n")
+  cat(SpaceSep1, formatC(CS / GT, width = CWidth, digits = digits, format = "f"), SpaceSep2, sep = " | ", collapse = "\n")
+  cat(RowSep1, rep(RowSep, ncol(t) + 1), sep = "|", collapse = "\n")
+
+  # Print Statistics
+
+  cat(rep("\n", 2))
+  cat("Tests for Independence of All Table Factors\n\n\n")
+
+  cat(CST$method,"\n\n")
+  cat("Chi^2 = ", CST$statistic, "    d.f. = ", CST$parameter, "    p = ", CST$p.value, "\n")
+
+  # Perform Fisher Tests
+  FTt <- fisher.test(t, alternative = "two.sided")
+
+  if (all(dim(t) == 2))
+  {
+    FTl <- fisher.test(t, alternative = "less")
+    FTg <- fisher.test(t, alternative = "greater")
+  }
+
+  cat(rep("\n", 2))
+  cat("Fisher's Exact Test for Count Data\n\n")
+
+  if (all(dim(t) == 2))
+  {
+    cat("Sample estimate odds ratio: ", FTt$estimate, "\n\n")
+
+    cat("Alternative hypothesis: true odds ratio is not equal to 1\n")
+    cat("p = ", FTt$p.value, "\n")
+    cat("95% confidence interval: ", FTt$conf.int, "\n\n")
+
+    cat("Alternative hypothesis: true odds ratio is less than 1\n")
+    cat("p = ", FTl$p.value, "\n")
+    cat("95% confidence interval: ", FTl$conf.int, "\n\n")
+
+    cat("Alternative hypothesis: true odds ratio is greater than 1\n")
+    cat("p = ", FTg$p.value, "\n")
+    cat("95% confidence interval: ", FTg$conf.int, "\n\n")
+  }
+  else
+  {
+    cat("Alternative hypothesis: two.sided\n")
+    cat("p = ", FTt$p.value, "\n")
+  }
+
+  cat(rep("\n", 2))
+}
+
