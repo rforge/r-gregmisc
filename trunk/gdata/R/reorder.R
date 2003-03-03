805a5a0d7cc31774da9a37e3b363782aeef3ed6e
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2003-03-03 17:24:21 +0000

    - Added handling of factor level names in addition to numeric indexes.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@164 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gdata/R/reorder.R
+++ trunk/gdata/R/reorder.R
@@ -1,7 +1,11 @@
 # $Id$
 #
 # $Log$
+# Revision 1.2  2003/03/03 17:24:21  warnes
+# - Added handling of factor level names in addition to numeric indexes.
+#
 # Revision 1.1  2002/08/01 18:06:41  warnes
+#
 # Added reorder() function to reorder the levels of a factor.
 #
 #
@@ -11,5 +15,8 @@
 reorder <- function( X, order )
   {
     if(!is.factor(X)) stop("reorder only handles factor variables")
-    factor( X, levels=levels(X)[order] )
+    if(is.numeric(order))
+      factor( X, levels=levels(X)[order] )
+    else
+      factor( X, levels=order )
   }
