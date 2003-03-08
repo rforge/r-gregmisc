7b546648d477305bb0f32b2316710c98bcda1e52
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2003-03-08 16:22:50 +0000

    - Added parameters for rotation of and amount of space allocated for   the row and column labels.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@169 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gplots/R/balloonplot.R
+++ trunk/gplots/R/balloonplot.R
@@ -1,7 +1,12 @@
 # $Id$
 #
 # $Log$
+# Revision 1.3  2003/03/08 16:22:47  warnes
+# - Added parameters for rotation of and amount of space allocated for
+#   the row and column labels.
+#
 # Revision 1.2  2003/01/20 18:35:47  warnes
+#
 # - Updated balloonplot help page.
 #
 # Revision 1.1  2003/01/03 21:34:06  warnes
@@ -43,6 +48,10 @@ balloonplot.default <- function(x,y,z,
                                 label=TRUE,
                                 label.digits=2,
                                 scale.method=c("volume","diameter"),
+                                colsrt=par("srt"),
+                                rowsrt=par("srt"),
+                                colmar=1,
+                                rowmar=1,
                                 ... )
 {
 
@@ -81,7 +90,7 @@ balloonplot.default <- function(x,y,z,
     }
   
   plot(x=as.numeric(x),
-       y=as.numeric(y),
+       y=nlevels(y) - as.numeric(y) + 1,
        cex=scale(z, max=dotsize, scale.method=scale.method),
        pch=dotchar, # plot character
        col=dotcolor, # dot color
@@ -90,13 +99,22 @@ balloonplot.default <- function(x,y,z,
        xaxt="n", # no x axis lables
        yaxt="n", # no y axis lables
        bty="n",  # no box around the plot
-       xlim=c(0,nlevels(x)+0.5), # extra space on either end of plot
-       ylim=c(0.5,nlevels(y)+1.5)  # so dots don't cross into margins
+       xlim=c(0.5-rowmar,nlevels(x)+0.5), # extra space on either end of plot
+       ylim=c(0.5,nlevels(y)+1.5+colmar-1),  # so dots don't cross into margins,
+       ...
      )
 
   # add text labels
-  text(x=1:nlevels(x), y=nlevels(y)+1, labels=levels(x))
-  text(y=1:nlevels(y), x=0,            labels=levels(y) )
+  par(adj=0.5)
+  text(x=1:nlevels(x),
+       y=nlevels(y)+(colmar/2)+0.5,
+       labels=levels(x),
+       srt=colsrt)
+
+  text(y=nlevels(y):1,
+       x=-rowmar/2+0.5,
+       labels=levels(y),
+       srt=rowsrt)
   
   # add borders between cells
   abline(v=(0:nlevels(x)+0.5))
@@ -105,7 +123,7 @@ balloonplot.default <- function(x,y,z,
   # annotate with actual values
   if(label)
     text(x=as.numeric(x),     # as.numeric give numeric values
-         y=as.numeric(y), 
+         y=nlevels(y) - as.numeric(y) + 1, 
          labels=format(z, digits=label.digits),       # label value
          col="black", # textt color
          )
