3bbfe2be324e7f99d2689187c93675027bcfff44
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2003-04-04 13:41:42 +0000

    - Added textplot.character to handle character strings. - Moved test for vector and matrix arguments to textplot.default. - Renamed arguments "col.margin" and "row.margin" to "cmar", and   "rmar" so that specifying "col='red'" is possible.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@178 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gplots/R/textplot.R
+++ trunk/gplots/R/textplot.R
@@ -1,24 +1,21 @@
 # $Id$
 #
 # $Log$
+# Revision 1.2  2003/04/04 13:41:42  warnes
+# - Added textplot.character to handle character strings.
+# - Moved test for vector and matrix arguments to textplot.default.
+# - Renamed arguments "col.margin" and "row.margin" to "cmar", and
+#   "rmar" so that specifying "col='red'" is possible.
+#
 # Revision 1.1  2003/04/02 22:29:53  warnes
+#
 # - Added textplot function and friends, as well as documentation.
 #
 #
 
 textplot <- function(object, halign="center", valign="center", cex, ... )
-  {
-    if (is.matrix(object) || (is.vector(object) && length(object)>1) )
-      textplot.matrix(object, halign, valign, cex, ... )  
-    else
-      UseMethod('textplot')
-  }
+  UseMethod('textplot')
 
-textplot.data.frame <- function(object,
-                             halign=c("center","center","right"),
-                             valign=c("center","top","bottom"),
-                             cex, ... )
-    textplot.matrix(object, halign, valign, cex, ... )  
 
 textplot.default <- function(object,
                              halign=c("center","left","right"),
@@ -26,7 +23,32 @@ textplot.default <- function(object,
                              cex, ... )
 {
 
-  results <- sprint(object)
+  if (is.matrix(object) || (is.vector(object) && length(object)>1) )
+    return(textplot.matrix(object, halign, valign, cex, ... ))
+
+
+  if(is.character(object))
+    object <- paste(object, collapse="\n")
+  else
+    object <- sprint(object)
+
+  textplot.character(object, halign,  valign, cex, ...)
+}
+
+
+textplot.data.frame <- function(object,
+                             halign=c("center","center","right"),
+                             valign=c("center","top","bottom"),
+                             cex, ... )
+    textplot.matrix(object, halign, valign, cex, ... )  
+
+
+
+textplot.character <- function(object,
+                             halign=c("center","left","right"),
+                             valign=c("center","top","bottom"),
+                             cex, ... )
+{
   
   halign=match.arg(halign)
   valign=match.arg(valign)
@@ -47,15 +69,15 @@ textplot.default <- function(object,
       for(i in 1:20)
         {
           oldcex <- cex
-          width <- strwidth(results,cex=cex)
-          height <- strheight(results,cex=cex)
+          width <- strwidth(object,cex=cex)
+          height <- strheight(object,cex=cex)
           cex <- oldcex/2 * (1 + 0.9/max(width,height) )
           if( abs(oldcex - cex) < 0.001) break
         }
     }
 
-  width  <- strwidth(results,cex=cex)
-  height <- strheight(results,cex=cex)
+  width  <- strwidth(object,cex=cex)
+  height <- strheight(object,cex=cex)
   
   # setup x alignment
   if(halign=="left")
@@ -75,7 +97,7 @@ textplot.default <- function(object,
     ypos <- 1 - (1-height)
     
   # make the plot
-  text( x=xpos, y=ypos, results, adj=c(0,1), cex=cex, ... )
+  text( x=xpos, y=ypos, object, adj=c(0,1), cex=cex, ... )
 
   invisible()
 }
@@ -84,7 +106,7 @@ textplot.default <- function(object,
 textplot.matrix <- function(object,
                             halign=c("center","left","right"),
                             valign=c("center","top","bottom"),
-                            cex, col.margin=2, row.margin=0.5,
+                            cex, cmar=2, rmar=0.5,
                             show.rownames=TRUE, show.colnames=TRUE,
                             hadj=1,
                             vadj=1,
@@ -138,10 +160,10 @@ textplot.matrix <- function(object,
                         apply( object, 2,
                               function(x) max(strwidth(x,cex=1) ) )
                         ) +
-                          strwidth('M', cex=cex) * col.margin * ncol(object)
+                          strwidth('M', cex=cex) * cmar * ncol(object)
         
           height <- strheight('M', cex=cex) * nrow(object) *
-                    (1 + row.margin)
+                    (1 + rmar)
 
           cex <- round(oldcex/2 * (1 + 0.95/max(width,height)), 2)
           if( abs(oldcex - cex) < 0.001) break
@@ -149,9 +171,9 @@ textplot.matrix <- function(object,
       }
 
   # compute the individual row and column heights
-  rowheight<-strheight("M",cex=cex) * (1 + row.margin)
+  rowheight<-strheight("M",cex=cex) * (1 + rmar)
   colwidth<- apply( object, 2, function(XX) max(strwidth(XX, cex=cex)) ) + 
-               strwidth("M")*col.margin
+               strwidth("M")*cmar
 
   
   width  <- sum(colwidth)
@@ -183,9 +205,9 @@ textplot.matrix <- function(object,
     for(j in 1:nrow(object)) {
       ypos<-y-(j-1)*rowheight
       if( (show.rownames && i==1) || (show.colnames && j==1) )
-        text(xpos, ypos, object[j,i], adj=c(hadj,vadj), cex=cex, font=2 )
+        text(xpos, ypos, object[j,i], adj=c(hadj,vadj), cex=cex, font=2, ... )
       else
-        text(xpos, ypos, object[j,i], adj=c(hadj,vadj), cex=cex, font=1 )
+        text(xpos, ypos, object[j,i], adj=c(hadj,vadj), cex=cex, font=1, ... )
     }
   }
 
