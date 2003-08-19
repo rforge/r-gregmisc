334a421e4c811aafab97429711a03693d476d6f8
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2003-08-19 14:58:09 +0000

    - Copy of local changes.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@219 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gplots/R/heatmap.2.R
+++ trunk/gplots/R/heatmap.2.R
@@ -1,3 +1,68 @@
+# $Id$
+
+
+###
+# Utility Functions
+###
+
+# Scale data to [0,1]
+foldchange.scale01 <- function(x, low=min(x), high=max(x) )
+  {
+    x <- ifelse( abs(x) >= 1, x, NA)
+    x <- ifelse( x>0, x-1, x+1)
+    x <- (x-low)/(high - low)
+    x
+  }
+
+scale01 <- function(x, low=min(x), high=max(x) )
+  {
+    x <- (x-low)/(high - low)
+    x
+  }
+
+# detect odd/even integers
+odd <- function(x) x!=as.integer(x/2)*2
+even <- function(x) x==as.integer(x/2)*2 
+
+# Generate red-to-green colorscale
+redgreen <- function(n)
+  {
+    if(even(n)) warning("n is even: colors will not be symmetric about black")
+    c(
+      hsv(h=0/6, v=seq(1,0,length=round((n+1)/2) )),
+      hsv(h=2/6, v=seq(0,1,length=1 + n - round((n+1)/2))[-1] ),
+      )
+  }
+
+# Vice versa
+greenred <- function(...)
+  {
+    rev(redgreen(...))
+  }
+
+# Compute foldchange from log-ratio values
+logratio2foldchange <- function(logratio, base=2)
+  {
+    retval <- base^(logratio)
+    retval <- ifelse(retval < 1, -1/retval, retval)
+    retval
+  }
+
+foldchange2logratio <- function(foldchange, base=2)
+  {
+    retval <- ifelse( foldchange<0, 1/-foldchange, foldchange)
+    retval <- log(retval,base)
+    retval
+  }
+
+
+
+
+
+
+###
+# Enhanced heatmap function
+###
 heatmap.2 <- function (x,
                        Rowv,
                        Colv,
@@ -7,21 +72,30 @@ heatmap.2 <- function (x,
                        scale = c("row", "column", "none"),
                        na.rm = TRUE,
                        col=redgreen(255),
-                       dendogram = c("both","row","column"),
+                       dendogram = c("both","row","column","none"),
                        rowlabel.space = 5,
                        collabel.space = 5,
                        legend.show = TRUE,
-                       trace=c("no","column","row","both"),
+                       trace=c("none","column","row","both"),
                        breaks,
-                       ...) 
+                       density.info=c("histogram","density"),
+                       colsep,
+                       rowsep,
+                       cellnote,
+                       notecex=0.75,
+                       scale01=foldchange.scale01,
+                       hline,
+                       vline,
+                       min.scale=min(breaks),
+                       max.scale=max(breaks),
+                       ...
+                       ) 
 {
     scale <- match.arg(scale)
     dendogram <- match.arg(dendogram)
     trace <- match.arg(trace)
-
-    myscale <- function(x, low=min(x), high=max(x) )
-      
-
+    density.info <- match.arg(density.info)
+    
     
     if (length(di <- dim(x)) != 2 || !is.numeric(x)) 
         stop("`x' must be a numeric matrix")
@@ -84,24 +158,41 @@ heatmap.2 <- function (x,
         4), heights = c(1, 4), respect = TRUE)
     par(mar = c(collabel.space, 0, 0, rowlabel.space))
     if(missing(breaks))
-      breaks <- seq( -max(abs(x)), max(abs(x)), length=length(col)+1 )
+      breaks <- seq( min(x), max(x), length=length(col)+1 )
 
     image(1:ncol(x), 1:nrow(x), t(x), axes = FALSE, xlim = c(0.5, 
         ncol(x) + 0.5), ylim = c(0.5, nrow(x) + 0.5), xlab = "", 
         ylab = "", col=col, breaks=breaks,
           ...)
 
+    if(!missing(colsep))
+      rect(xleft=colsep+0.5,   ybottom=rep(0,length(colsep)),
+           xright=colsep+0.55, ytop=rep(nrow(x)+1,colsep),
+           lty=1, lwd=1, col="white", border="white")
+
+    if(!missing(rowsep))
+      rect(xleft=0,          ybottom=nrow(x)+1-rowsep-0.5,
+           xright=ncol(x)+1, ytop=nrow(x)+1-rowsep-0.55,
+           lty=1, lwd=1, col="white", border="white")
+
+    if(!missing(cellnote))
+      text(x=c(col(cellnote)),
+           y=nrow(cellnote)+1-c(row(cellnote)), labels=c(cellnote),
+           col="yellow", cex=notecex)
     
     # show traces
-    tmp <- (x - min(x))/(max(x) - min(x))  # scale to [0,1]
+    x.scaled <- scale01(x, min.scale, max.scale)
 
     if(trace %in% c("both","column") )
       {
         for( i in colInd )
           {
-            abline(v=c(i-0.5,i+0.5), col='black', lty=1)
-            abline(v=, col='black', lty=2)
-            xv <- rep(i, nrow(tmp)) + tmp[,i] - 0.5
+            if(!missing(vline))
+              {
+                vline.vals <- scale01(vline, min.scale, max.scale)
+                abline(v=i-0.5 + vline.vals, col='black', lty=2)
+              }
+            xv <- rep(i, nrow(x.scaled)) + x.scaled[,i] - 0.5
             xv <- c(xv[1], xv)
             yv <- 1:length(xv)-0.5
             lines(x=xv, y=yv, lwd=1, col='yellow', type="s")
@@ -112,37 +203,45 @@ heatmap.2 <- function (x,
       {
         for( i in rowInd )
           {
-            abline(h=c(i-0.5,i+0.5), col='black', lty=1)
-            abline(h=i, col='black', lty=2)
-            yv <- rep(i, ncol(tmp)) + tmp[i,] - 0.5
+            if(!missing(hline))
+              {
+                hline.vals <- scale01(hline, min.scale, max.scale)
+                #abline(v=i-0.5 + vline, col='black', lty=2)
+                # FIXME
+              }
+            yv <- rep(i, ncol(x.scaled)) + x.scaled[i,] - 0.5
             yv <- rev(c(yv[1], yv))
             xv <- length(yv):1-0.5
             lines(x=xv, y=yv, lwd=1, col='yellow', type="s")
           }
       }
 
-
-
     # add axes
-    axis(1, 1:ncol(x), las = 2, line = -0.5, tick = 0, labels = if (is.null(colnames(x))) 
-        (1:ncol(x))[colInd]
-    else colnames(x), cex.axis = c.cex)
-    axis(4, 1:nrow(x), las = 2, line = -0.5, tick = 0, labels = if (is.null(rownames(x))) 
-        (1:nrow(x))[rowInd]
-    else rownames(x), cex.axis = r.cex)
+    if (is.null(colnames(x))) {
+      axis(1, 1:ncol(x), las = 2, line = -0.5, tick = 0,
+           labels = (1:ncol(x))[colInd])
+    } else {
+      axis(1, 1:ncol(x), las = 2, line = -0.5, tick = 0,
+           colnames(x), cex.axis = c.cex)
+    }
+    
+    if (is.null(rownames(x))) {
+      axis(4, 1:nrow(x), las = 2, line = -0.5, tick = 0,
+           labels = (1:nrow(x))[rowInd])
+    } else {
+      axis(4, 1:nrow(x), las = 2, line = -0.5, tick = 0,
+           rownames(x), cex.axis = r.cex)
+    }
+    
     if (!missing(add.expr)) 
         eval(substitute(add.expr))
 
-
-
-    
     par(mar = c(collabel.space, 3, 0, 0))
 
     if( dendogram %in% c("both","row") )
       plot(ddr, horiz = TRUE, axes = FALSE, yaxs = "i", leaflab = "none")
     else
       plot.new()
-    
     
     par(mar = c(0, 0, 5, rowlabel.space ) )
 
@@ -155,23 +254,36 @@ heatmap.2 <- function (x,
     
     if(legend.show)
       {
-        z <- seq(min(breaks),max(breaks),length=1000)
-        image( z=matrix(z, ncol=1),
+        z <- breaks[-1]#seq(min(breaks),max(breaks),length=100)
+        image(z=matrix(z, ncol=1),
               col=col, breaks=breaks,
               xaxt="n", yaxt="n" )
 
-        xv <- seq(0,1,length=7) 
-        lv <- pretty(quantile(breaks))
-        axis(1,at=xv,labels=format(lv, digits=2) )
+        par(usr=c(0,1,0,1))
+        lv <- pretty(seq(min.scale,max.scale,length=5))
+        xv <- scale01(as.numeric(lv), min.scale, max.scale)
+        axis(1, at=xv, labels=lv)
 
-        # Experimental : also plot density of data
-        dens <- density(x, adjust=0.25)
-        omit <- dens$x < min(breaks) | dens$x > min(breaks)
-        dens$x <- dens$x[-omit]
-        dens$y <- dens$y[-omit]
-        dens$x <- (dens$x - min(dens$x) )/ (max(dens$x)-min(dens$x))
-        dens$y <- dens$y / max(dens$y) * par("usr")[4] * 0.95
-        lines(dens$x, dens$y, col="yellow", lwd=1)
+        if(density.info=="density")
+          {
+            # Experimental : also plot density of data
+            dens <- density(x, adjust=0.25)
+            omit <- dens$x < min(breaks) | dens$x > min(breaks)
+            dens$x <- dens$x[-omit]
+            dens$y <- dens$y[-omit]
+            dens$x <- scale01(dens$x)
+            dens$y <- dens$y / max(dens$y) * 0.95
+            lines(dens$x, dens$y, col="yellow", lwd=1)
+          }
+        else if(density.info=="histogram")
+          {
+            h <- hist(x, plot=F, breaks=breaks)
+            x <- h$breaks
+            x <- scale01(breaks,min.scale,max.scale)
+            y <- c(h$density, h$density[length(h$density)]) 
+            y <- y/max(y) * 0.95
+            lines(x, y, col="yellow", lwd=1, type="s")
+          }
         
       }
     
