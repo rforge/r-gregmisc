6d7d3111d096fc6b7d4a3d6d39c779cc9e560fe6
(no author) <(no author)@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2003-12-09 21:35:32 +0000

    This commit was manufactured by cvs2svn to create tag 'r_1_0_0'.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@260 45178186-c70b-4eec-b05e-9f6c5a094bf4

---
+++ tags/r_1_0_0/fork/R/signame.R
@@ -0,0 +1,40 @@
+# %Id$
+
+signame <- function(val)
+  {
+    retval <- .C("Rfork_signame",
+                 name=character(1),
+                 val=as.integer(val),
+                 desc=character(1))
+    unlist(retval)
+  }
+
+sigval <- function(name)
+  {
+    name <- toupper(name)
+    if( length(grep("SIG",name))!=1 )
+       name <- paste("SIG", name, sep="")
+    retval <- .C("Rfork_siginfo",
+                 name=name,
+                 val=integer(1),
+                 desc=character(1))
+    unlist(retval)
+  }
+
+siglist <- function()
+  {
+    retval <- .Call("Rfork_siglist")
+    retval <- as.data.frame(retval)
+    names(retval) <- c("name","val","desc")
+    retval
+  }
+
+#siginfo <- function(val, name)
+#  {
+#    if(missing(val) && missing(name))
+#      return(siglist())
+#    else if (missing(val))
+#      return(sigval(name))
+#    else
+#      return(signame(val))
+#  }
---
+++ tags/r_1_0_0/fork/R/wait.R
@@ -0,0 +1,17 @@
+# $Id$
+
+wait <- function(pid, nohang=FALSE, untraced=FALSE)
+{
+  if(missing(pid) || is.null(pid)) 
+    retval <-   .C("Rfork_wait", pid=integer(1), status=integer(1) )
+  else
+    retval <- .C("Rfork_waitpid",
+                 pid = as.integer(pid),
+                 as.integer(nohang),
+                 as.integer(untraced),
+                 status=integer(1) )
+
+  return(c("pid"=retval$pid,
+           "status"=retval$status))
+}
+
