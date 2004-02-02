5fd82364d2d191f278b6e9d193e1230b4e389399
(no author) <(no author)@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2004-02-02 16:26:26 +0000

    This commit was manufactured by cvs2svn to create tag 'gregmisc_0_8_9'.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@288 45178186-c70b-4eec-b05e-9f6c5a094bf4

---
+++ tags/gregmisc_0_8_9/gplots/R/qqnorm.aov.R
@@ -0,0 +1,51 @@
+# $Id$
+#
+# $Log$
+# Revision 1.3  2003/03/07 15:48:35  warnes
+# - Minor changes to code to allow the package to be provided as an
+#   S-Plus chapter.
+#
+# Revision 1.2  2003/01/02 15:40:09  warnes
+# - Renamed first parameter to match qqnorm generic.
+#
+# Revision 1.1  2002/12/31 19:50:58  warnes
+# Initial checkin of qqnorm.aov function and documentation submitted by
+# Kjetil Halvorsen <kjetilh@jupiter.umsanet.edu.bo>.
+#
+#
+
+if(is.R())
+qqnorm.aov <- function (y, full = FALSE, label = FALSE, omit = NULL,
+                        xlab = paste(if(full) "" else "Half", " Normal plot"),
+                        ylab = "Effects", ...)
+{
+    r <- y$rank
+    eff <- if (full)
+        effects(y, set.sign = TRUE)[1:r]
+    else abs(effects(y))[1:r]
+    na <- names(eff)
+    int <- match("(Intercept)", na)
+    if (!is.null(omit)) {
+        if (is.character(omit)) {
+            int <- c(int, match(omit, na))
+        }
+        else int <- c(int, omit)
+    }
+    int <- int[!is.na(int)]
+    if (length(int))
+        eff <- eff[-int]
+    n <- length(eff)
+    if (n <= 0)
+        stop("Not enough effects")
+    ord <- order(eff)
+    na <- names(eff)
+    P <- if (full)
+        ppoints(n)
+    else ((1:n) + n)/(2 * n + 1)
+    Q <- qnorm(P)
+    plot(x = Q, y = eff[ord], xlab = xlab, ylab = ylab, ...)
+    if (label && dev.interactive())
+        identify(Q, eff[ord], names(eff)[ord])
+}
+
+
