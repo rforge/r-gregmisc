f0e7050559f4ae8a643505de9d68302dd87f5eaf
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2004-03-25 18:17:43 +0000

    Add enhancements to estimable() provided by S�ren H�jsgaard \email{sorenh@agrsci.dk}:
      I have made a modified version of the function [..] which
      1) also works on geese and gee objects and
      2) can test hypotheses af the forb L * beta = beta0 both as a
      single Wald test and row-wise for each row in L.
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@295 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gmodels/R/estimable.R
+++ trunk/gmodels/R/estimable.R
@@ -1,124 +1,178 @@
-# $Id$
-#
-# $Log$
-# Revision 1.4  2002/04/09 00:51:29  warneg
-# Checkin for version 0.5.3
-#
-# Revision 1.3  2001/12/18 22:12:28  warneg
-#
-# - Modified to make confidence intervals optional.  Changed 'alpha'
-#   parameter giving significance level to 'conf.int' giving confidence
-#   level.
-#
-# Revision 1.2  2001/12/18 21:27:37  warneg
-#
-# - Modified to work correctly when obj is of class 'aov' by specifying
-#   summary.lm instead of summary.  This ensures that the summary object
-#   has the fields we need.
-#
-# Revision 1.1  2001/12/10 19:26:52  warneg
-# renamed from contrast.coeff.R to estimable.R (incorrectly via contrast.lm.R)
-#
-# Revision 1.6  2001/12/10 19:20:57  warneg
-# Renamed back to estimable
-#
-# Revision 1.3  2001/12/07 20:32:13  warneg
-# - If cm is not matrix, coerce it into a single-row matrix.
-#
-# Revision 1.2  2001/12/07 20:07:22  warneg
-#
-# Substantial enhancements to initial version:
-#
-#  - Added StdErr, t-value, p-value, and DF columns to output.
-#  - Modified to use t distribution rather than normal distribution for
-#    confidence intervals.
-#
-#
-
-estimable <- function( obj, cm, conf.int=NULL )
+new.estimable <- function (obj, cm, beta0, conf.int=NULL, joint.test=FALSE,
+                           show.beta0) 
 {
-
-  if( !is.matrix(cm) && !is.data.frame(cm) )
+  if (!is.matrix(cm) && !is.data.frame(cm)) 
     cm <- matrix(cm, nrow=1)
 
-  # Function to compute arbitrary contrasts with c.i.
-  # from a linear model ( lm, glm or nlme )
-  # Original version by BXC (Bendix Carstensen) 12/01.
-  # Modified by Gregory R. Warnes 2001-12-06 and subsequent
-  if ( "lme" %in% class( obj ) ) 
+  if(missing(show.beta0))
     {
-      cf  <- summary(obj)$tTable
-      rho <- summary(obj)$cor  
-      vcv <- rho * outer(cf[,2],cf[,2])
-      tmp <- cm
-      tmp[tmp==0] <- NA
-      df.all <- t(abs( t(tmp) * obj$fixDF$X))
-      df <- apply( df.all, 1, min, na.rm=TRUE)
-      problem <- apply( df.all != df, 1, any, na.rm=TRUE)
-      if( any(problem) )
-        warning(paste("Degrees of freedom vary among parameters used to ",
-                      "construct linear contrast(s): ",
-                      paste((1:nrow(tmp))[problem],collapse=","),
-                      ". Using the smallest df among the set of parameters.",
-                      sep=""))
+      if(!missing(beta0))
+        show.beta0=TRUE
+      else
+        show.beta0=FALSE
     }
-  else if ( "lm" %in% class( obj ) )
+       
+  
+  if (missing(beta0))
     {
-      cf  <- summary.lm(obj)$coefficients
-      vcv <- summary.lm(obj)$cov.unscaled * summary.lm(obj)$sigma^2
-      if ("glm" %in% class( obj ) ) 
-        vcv <- summary(obj)$cov.scaled
-      df <- obj$df.residual
+      beta0 = rep(0, ifelse(is.null(nrow(cm)), 1, nrow(cm)))
+
+    }
+  
+  
+  if (joint.test==TRUE)
+    {
+      .wald(obj, cm, beta0)
     }
   else
     {
-      stop("obj must be of class 'lm', 'glm', 'aov', 'lme', or 'nlme'")
+      if ("lme" %in% class(obj)) {
+        stat.name <- "t.stat"
+        cf <- summary(obj)$tTable
+        rho <- summary(obj)$cor
+        vcv <- rho * outer(cf[, 2], cf[, 2])
+        tmp <- cm
+        tmp[tmp==0] <- NA
+        df.all <- t(abs(t(tmp) * obj$fixDF$X))
+        df <- apply(df.all, 1, min, na.rm=TRUE)
+        problem <- apply(df.all !=df, 1, any, na.rm=TRUE)
+        if (any(problem)) 
+          warning(paste("Degrees of freedom vary among parameters used to ", 
+                        "construct linear contrast(s): ", 
+                        paste((1:nrow(tmp))[problem], collapse=", "), 
+                        ". Using the smallest df among the set of parameters.", 
+                        sep=""))
+      }
+      else if ("lm" %in% class(obj))
+        {
+          stat.name <- "t.stat"
+          cf <- summary.lm(obj)$coefficients
+          vcv <- summary.lm(obj)$cov.unscaled * summary.lm(obj)$sigma^2
+          df <- obj$df.residual
+          if ("glm" %in% class(obj))
+            {
+              vcv <- summary(obj)$cov.scaled
+              if(family(obj)[1] %in% c("poisson", "binomial"))
+                {
+                  stat.name <- "X2.stat"            
+                  df <- 1
+                }
+              else
+                {              
+                  stat.name <- "t.stat"
+                  df <- obj$df.residual
+                }
+            }
+        }
+      else if ("geese" %in% class(obj))
+        {
+          stat.name <- "X2.stat"
+          cf <- summary(obj)$mean
+          vcv <- obj$vbeta
+          df <- 1
+        }
+      else if ("gee" %in% class(obj))
+        {
+          stat.name <- "X2.stat"
+          cf <- summary(obj)$coef
+          vcv <- obj$robust.variance
+          df <- 1
+        }
+      else
+        {
+          stop("obj must be of class 'lm', 'glm', 'aov', 'lme', 'gee', 'geese' or 'nlme'")
+        }
+      if (is.null(cm)) 
+        cm <- diag(dim(cf)[1])
+      if (!dim(cm)[2]==dim(cf)[1]) 
+        stop(paste("\n Dimension of ", deparse(substitute(cm)), 
+                   ": ", paste(dim(cm), collapse="x"),
+                   ", not compatible with no of parameters in ", 
+                   deparse(substitute(obj)), ": ", dim(cf)[1], sep=""))
+      ct <- cm %*% cf[, 1] 
+      ct.diff <- cm %*% cf[, 1] - beta0
+      
+      vc <- sqrt(diag(cm %*% vcv %*% t(cm)))
+      if (is.null(rownames(cm))) 
+        rn <- paste("(", apply(cm, 1, paste, collapse=" "), 
+                    ")", sep="")
+      else rn <- rownames(cm)
+      switch(stat.name, 
+             t.stat={
+               prob <- 2 * (1 - pt(abs(ct.diff/vc), df))
+             }, 
+             X2.stat={
+               prob <- 1 - pchisq((ct.diff/vc)^2, df=1)
+             })
+      
+      #if (length(which(c("glm", "geese") %in% class(obj))) > 0){
+      if (stat.name=="X2.stat")
+        {
+          retval <- cbind(hyp=beta0, est=ct, stderr=vc, t.value=ct.diff/vc, 
+                          df=df, prob=1 - pchisq((ct.diff/vc)^2, df=1))
+          dimnames(retval) <- list(rn, c("beta0", "Estimate", "Std.Error", 
+                                         "X2.value", "DF", "Pr(>|X^2|)"))
+        }
+      #else if (length(which(c("lm", "lme", "aov", "nlme") %in%  class(obj))) > 0) {
+      else if (stat.name=="t.stat")
+        {
+          retval <- cbind(hyp=beta0, est=ct, stderr=vc, t.value=ct.diff/vc, 
+                          df=df, prob=2 * (1 - pt(abs(ct.diff/vc), df)))
+          dimnames(retval) <- list(rn, c("beta0", "Estimate", "Std.Error", 
+                                         "t.value", "DF", "Pr(>|t|)"))
+        }
+      
+      if (!is.null(conf.int))
+        {
+          if (conf.int <=0 || conf.int >=1) 
+            stop("conf.int should be betweeon 0 and 1. Usual values are 0.95, 0.90")
+          alpha <- 1 - conf.int
+          switch(stat.name, t.stat={
+            quant <- qt(1 - alpha/2, df)
+          }, X2.stat={
+            quant <- qt(1 - alpha/2, 100)
+          })
+          nm <- c(colnames(retval), "Lower.CI", "Upper.CI")
+          retval <- cbind(retval, lower=ct.diff - vc * quant, upper=ct.diff + 
+                          vc * quant)
+          colnames(retval) <- nm
+        }
+      retval <- as.data.frame(retval)
+      if(!show.beta0) retval$beta0 <- NULL
+      return(retval)
     }
-  
+  }
 
-  if ( is.null( cm ) ) cm <- diag( dim( cf )[1] )
-  if ( !dim( cm )[2]==dim( cf )[1] ) stop(  
-                   paste( "\n Dimension of ",
-                         deparse( substitute( cm ) ), ": ",
-                         paste( dim(cm), collapse="x" ),
-                         ", not compatible with no of parameters in ",
-                         deparse( substitute( obj ) ), ": ",
-                         dim(cf)[1], sep="" ) )
-  ct <- cm %*% cf[,1]
-  vc <- sqrt( diag( cm %*% vcv %*% t(cm) ) )
-
-
-  
-  retval <- cbind(est=ct,
-                  stderr=vc,
-                  t.value=ct/vc,
-                  df=df,
-                  prob= 2 * (1 - pt(abs(ct/vc), df) )
-                  )
-
-  if (is.null(rownames(cm)))
-    rn <- paste("(",apply(cm,1,paste,collapse=" "),")",sep="")
-  else
-    rn <- rownames(cm)
-
-  dimnames(retval) <- list( rn, c("Estimate","Std. Error", 
-                                  "t value",
-                                  "DF",
-                                  "Pr(>|t|)" ) )
-
-  if (!is.null(conf.int))
-    {
-      if( conf.int <= 0 || conf.int >= 1 )
-        stop("conf.int should be betweeon 0 and 1. Usual values are 0.95, 0.90")
-      alpha <- 1 - conf.int
-      nm <- c(colnames(retval), "Lower CI", "Upper CI")
-      retval <- cbind(retval,
-                      lower=ct - vc * qt(1-alpha/2, df),
-                      upper=ct + vc * qt(1-alpha/2, df) )
-      colnames(retval) <- nm
+.wald <- function (obj, cm, beta0=rep(0, ifelse(is.null(nrow(cm)), 1, nrow(cm)))) 
+{
+    if (!is.matrix(cm) && !is.data.frame(cm)) 
+        cm <- matrix(cm, nrow=1)
+    df <- nrow(cm)
+    if ("geese" %in% class(obj))
+{
+        cf <- obj$beta
+        vcv <- obj$vbeta
     }
-
-  retval
+    else if ("gee" %in% class(obj))
+{
+        cf <- summary(obj)$coef
+        vcv <- obj$robust.variance
+    }
+    else if ("lm" %in% class(obj))
+{
+        cf <- summary.lm(obj)$coefficients[, 1]
+        vcv <- summary.lm(obj)$cov.unscaled * summary.lm(obj)$sigma^2
+        if ("glm" %in% class(obj))
+{
+            vcv <- summary(obj)$cov.scaled
+        }
+    }
+    else stop("obj must be of class 'lm', 'glm', 'aov', 'gee' or 'geese'")
+    u <- (cm %*% cf)-beta0
+    vcv.u <- cm %*% vcv %*% t(cm)
+    W <- t(u) %*% solve(vcv.u) %*% u
+    prob <- 1 - pchisq(W, df=df)
+    retval <- as.data.frame(cbind(W, df, prob))
+    names(retval) <- c("X2.stat", "DF", "Pr(>|X^2|)")
+    print(as.data.frame(retval))
 }
-
-
