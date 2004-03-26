e3b95c5cbc67fa58b36c63aac320f44068dccc4b
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2004-03-26 22:12:47 +0000

    More of the same.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@299 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gtools/R/running.R
+++ trunk/gtools/R/running.R
@@ -1,81 +1,87 @@
 # $Id$
 #
-# $Log$
-# Revision 1.6  2004/03/26 15:34:26  warnes
-# Fix bug discovered by Sean Davis <sdavis2@mail.nih.gov>.  The running
-# function took an improper shortcut.  When allow.fewer=FALSE it was
-# still passing shorter lists of elements to the called function, and
-# then overwriting the results for the shorter lists with NAs.  The code
-# now skips evaluation of the function on lists shorter than the
-# specified length when allow.fewer=FALSE.
-#
-# Revision 1.5  2002/09/24 14:57:19  warnes
-#
-# - Fixed error where running was always calling running2 with
-#   'fun=mean', ignoring the specified funtion.
-#
-# Revision 1.4  2002/08/01 19:37:14  warnes
-#
-# - Corrected documentation mismatch for ci, ci.default.
-#
-# - Replaced all occurences of '_' for assignment with '<-'.
-#
-# - Replaced all occurences of 'T' or 'F' for 'TRUE' and 'FALSE' with
-#   the spelled out version.
-#
-# - Updaded version number and date.
-#
-# Revision 1.3  2002/03/07 23:38:37  warneg
-#
-# - Added "running2", which handles both univariate and bivariate cases
-# - Modified "running" to call "running2"
-#
-# Revision 1.2  2001/09/01 00:01:54  warneg
-# Release 0.3.0
-#
-# Revision 1.1  2001/08/25 05:53:37  warneg
-# Initial CVS checkin.
-#
-#
 
-"running" <- function( X, fun=mean, width=min(length(X),20),
-                     allow.fewer=FALSE,...)
-  running2(X=X, fun=fun, width=width, allow.fewer=allow.fewer, ...)
-
-"running2" <- function( X, Y=NULL, fun=mean, width=min(length(X),20),
-                     allow.fewer=FALSE,...)
+"running" <- function(X, Y=NULL,
+                      fun=mean,
+                      width=min(length(X), 20),
+                      allow.fewer=FALSE, pad=FALSE,
+                      align=c("right", "center", "left"),
+                      ...)
 {
+  align=match.arg(align)
+  
   n <- length(X)
 
-  from  <-  sapply( (1:n) - width + 1, function(x) max(x,1) )
-  to    <-  1:n
+  if (align=="left")
+    {
+      from  <-  1:n 
+      to    <-  pmin( (1:n)+width-1, n)
+    }
+  else if (align=="right")
+    {
+      from  <-  pmax( (1:n)-width+1, 1)
+      to    <-  1:n 
+    }
+  else #align=="center"
+    {
+      from <-  pmax((2-width):n,1)
+      to   <-  pmin(1:(n+width-1),n)
+      if(!odd(width)) stop("width must be odd for center alignment")
+      
+    }
+
+  elements  <- apply(cbind(from, to), 1, function(x) seq(x[1], x[2]) )
+
+  if(is.matrix(elements))
+    elements <- as.data.frame(elements) # ensure its a list!
+
+  names(elements) <- paste(from,to,sep=':')
 
   if(!allow.fewer)
     {
-      from <- from[ -(1:(width-1)) ]
-      to <- to[ -(1:(width-1)) ]
+      len <- sapply(elements, length)
+      skip <- (len < width)
+    }
+  else
+    {
+      skip <- 0
     }
   
-  elements  <- apply(cbind(from,to), 1,function(x) seq(x[1], x[2]) )
 
-  if(is.matrix(elements))
-    elements  <- as.data.frame(elements)
-
+  run.elements  <- elements[!skip]
+  
   if(is.null(Y))  # univariate 
     {
       funct <- function(which,what,fun,...) fun(what[which],...)
       
-      Xvar <- sapply(elements, funct, what=X, fun=fun, ...)
+      Xvar <- sapply(run.elements, funct, what=X, fun=fun, ...)
     }
   else # bivariate
     {
       funct <- function(which,XX,YY,fun,...) fun(XX[which],YY[which], ...)
       
-      Xvar <- sapply(elements, funct, XX=X, YY=Y, fun=fun, ...)
+      Xvar <- sapply(run.elements, funct, XX=X, YY=Y, fun=fun, ...)
     }
 
   
-  names(Xvar) <- paste(from,to,sep=":")
+  if(allow.fewer || !pad)
+      return(Xvar)
+    
+  if(is.matrix(Xvar))
+    {
+      wholemat <- matrix( new(class(Xvar[1,1]), NA),
+                         ncol=length(to), nrow=nrow(Xvar) )
+      colnames(wholemat) <- paste(from,to,sep=':')
+      wholemat[,-skip] <- Xvar
+      Xvar <- wholemat
+    }
+  else
+    {
+      wholelist <- rep(new(class(Xvar[1]),NA),length(from))
+      names(wholelist) <-  names(elements)
+      wholelist[ names(Xvar) ] <- Xvar
+      Xvar <- wholelist
+    }
+  
+  return(Xvar)}
 
-  return(Xvar)
-}
