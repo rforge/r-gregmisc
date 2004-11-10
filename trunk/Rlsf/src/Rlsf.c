db0ecb9dbed0bee08db46c188444e9cd6cc57910
csmith <csmith@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2004-11-10 23:49:26 +0000

    - have to set rLimits in the submit structure to "unlimited"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@487 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/Rlsf/src/Rlsf.c
+++ trunk/Rlsf/src/Rlsf.c
@@ -22,12 +22,14 @@ lsf_initialize(void)
 SEXP
 lsf_job_submit(SEXP sexp_debug, SEXP sexp_command)
 {
-  int jobId, debug;
+  int jobId, debug, i;
   struct submit submitRequest;
   struct submitReply submitReply;
 
   debug = INTEGER(sexp_debug)[0];
   memset(&submitRequest, 0, sizeof(submitRequest));
+  for (i = 0; i < LSF_RLIM_NLIMITS; i++)
+    submitRequest.rLimits[i] = DEFAULT_RLIM;
   submitRequest.command = CHAR(STRING_ELT(sexp_command, 0));
   submitRequest.options |= SUB_OUT_FILE;
   if (debug) {
