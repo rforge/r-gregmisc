d769b20b79be1387e0cb1bd11e0a2cfe2134d78d
csmith <csmith@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2004-11-11 02:08:49 +0000

    - makeLSFcluster starts an Rmpi snow cluster of lsf.numcpu() slaves
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@494 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/Rlsf/R/makeLSFcluster.R
+++ trunk/Rlsf/R/makeLSFcluster.R
@@ -1,6 +1,9 @@
 makeLSFcluster <- function()
 {
-  ncpus <- lsf.numcpu()
+  if (!require(Rmpi))
+    stop("The 'Rmpi' package is needed for parallel LSF jobs.")
+  if (!require(snow))
+    stop("The 'snow' package could not be found.")
   
   if (mpi.comm.size() != 0) {
     cl <-getMPIcluster()
@@ -8,7 +11,7 @@ makeLSFcluster <- function()
       cl <- makeCluster(type="MPI") 
   }
   else {
-    cl <- makeCluster(ncpus - 1, type="MPI")
+    cl <- makeCluster(lsf.numcpu(), type="MPI")
   }
 
   return(cl)
