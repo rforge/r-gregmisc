09862c11fae47dca551a456a7b1ee01bc597f2b0
(no author) <(no author)@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2005-03-31 18:06:21 +0000

    This commit was manufactured by cvs2svn to create tag 'release_1_0_2'.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@585 45178186-c70b-4eec-b05e-9f6c5a094bf4

---
+++ tags/release_1_0_2/fork/R/fork.R
@@ -0,0 +1,39 @@
+# $Id$
+
+fork <- function(slave)
+  
+  {
+    if(missing(slave) || class(slave)!="function" && !is.null(slave) )
+      stop("function for slave process to exectute must be provided.")
+
+    pid  <- .C("Rfork_fork",
+               pid=integer(1),
+               PACKAGE="fork"
+               )$pid
+
+    if(pid==0)
+      {
+        # the slave shouldn't get a list of the master's children (?)
+        if(exists(".pidlist",where="package:fork"))
+          remove(".pidlist",pos="package:fork")
+        
+        if(!is.null(slave))
+          {
+            on.exit( { cat("ERROR. Calling exit()..."); exit(); }  );
+            slave();
+            exit();
+          }
+      }
+    else
+      {
+        # save all the pid's that get created just in case the user forgets
+        # to keep track of them!
+        if(!exists(".pidlist",where="package:fork"))
+          assign(".pidlist",pid,pos="package:fork")
+        else
+          assign(".pidlist",c(.pidlist, pid),pos="package:fork")
+      }
+
+    return(pid)
+  }
+    
