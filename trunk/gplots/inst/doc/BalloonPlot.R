eed730c1dfbc5ca8c8f8f361abc1bad67a4cf2a9
nj7w <nj7w@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2005-11-09 22:29:30 +0000

    Updated code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@730 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gplots/inst/doc/BalloonPlot.R
+++ trunk/gplots/inst/doc/BalloonPlot.R
@@ -12,7 +12,7 @@ survived <- dframe[dframe$Survived=="No",]
 attach(survived)
 
 pdf("Figure1.pdf")
-
+ 
 balloonplot(x=Class, y=list(Age, Sex), z=Freq,
             sort=TRUE, show.zeros=TRUE, cum.margins=FALSE,
             dotcol="white", show.margins=FALSE,
@@ -39,18 +39,28 @@ attach(survived)
 
 pdf("Figure2.pdf")
 
-balloonplot(x=Class, y=list(Age, Sex), z=Freq,
-            sort=TRUE, show.zeros=TRUE, cum.margins=FALSE,
-            main="BalloonPlot : Surviving passengers")
+balloonplot(x=Class,
+            y=list(Age, Sex),
+            z=Freq,
+            dotcol = "green",
+            sort=TRUE,
+            show.zeros=TRUE,
+            cum.margins=FALSE,
+            main="BalloonPlot : Surviving passengers"
+            )
+title(main=list("Area is proportional to number of passengers",
+           cex=0.9),
+      line=0.5)
 
 dev.off()
 
 detach(survived)
 
 
+####
+## Figure 3
+####
 
-
-## colorize: surviors lightblue, non-survivors: grey
 attach(dframe)
 colors <- ifelse( Survived=="Yes", "green", "magenta")
 
@@ -63,8 +73,54 @@ balloonplot(x=Class,
             dotcol = colors,
             show.zeros=TRUE,
             #show.margins=FALSE,
-            cum.margins=FALSE
+            cum.margins=FALSE,
+            main="BalloonPlot : Passenger Class by Survival, Age and Sex"
             )
+title(main=list("Area is proportional to number of passengers",
+           cex=0.9),
+      line=0.5)
+
+
+dev.off()
+
+detach(dframe)
+
+####
+## Figure 4
+####
+
+attach(dframe)
+colors <- ifelse( Survived=="Yes", "green", "magenta")
+
+pdf("Figure4.pdf")
+
+balloonplot(x=Class,
+            y=list(Survived, Age, Sex),
+            z=Freq,
+            zlab="Number of Passengers",
+            sort=TRUE,
+            dotcol=colors,
+            show.zero=TRUE,
+            main="BalloonPlot : Passenger Class by Survival, Age and Sex"
+            )
+
+
+title(main=list("Area is proportional to number of passengers",
+           cex=0.9),
+      line=0.5)
+
+legend(3,0.5,
+       legend=c("Not survived","Survived"),
+       col=c("magenta","green"),
+       pch=20,
+       cex=.8,
+       pt.cex=0.8,
+       text.col=c("magenta", "green"),
+       horiz=TRUE,
+       xjust=0.5,
+       bty="n"
+       )
+
 
 dev.off()
 
