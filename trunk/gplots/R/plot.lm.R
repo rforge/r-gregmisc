d52735329053ceb2b8741c2a8ae23b666e8c0753
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2005-12-02 21:11:18 +0000

    Remove .Alias calls, since .Alias is defunct.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@777 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gplots/R/plot.lm.R
+++ trunk/gplots/R/plot.lm.R
@@ -30,7 +30,7 @@ function(x, which = 1:5,
         hii <- lm.influence(x)$hat
         w <- weights(x)
         # r.w := weighted.residuals(x):
-        r.w <- if(is.null(w)) .Alias(r) else (sqrt(w)*r)[w!=0]
+        r.w <- if(is.null(w)) r else (sqrt(w)*r)[w!=0]
         rs <- r.w/(s * sqrt(1 - hii))
     }
     if (any(show[c(1,3)]))
@@ -95,7 +95,7 @@ function(x, which = 1:5,
 	sqrtabsr <- sqrt(abs(rs))
 	ylim <- c(0, max(sqrtabsr))
 	yl <- as.expression(substitute(sqrt(abs(YL)), list(YL=as.name(ylab23))))
-        yhn0 <- if(is.null(w)) .Alias(yh) else yh[w!=0]
+        yhn0 <- if(is.null(w)) yh else yh[w!=0]
 	plot(yhn0, sqrtabsr, xlab = l.fit, ylab = yl, main = main,
 	     ylim = ylim, type = "n", ...)
 	panel(yhn0, sqrtabsr, ...)
