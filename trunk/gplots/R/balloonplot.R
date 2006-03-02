5139b07ed9a1ee7b09f31e9bd6d9840254fcc04a
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2006-03-02 21:43:24 +0000

    Adjust row total placement to adjust for width of the totals
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@908 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gplots/R/balloonplot.R
+++ trunk/gplots/R/balloonplot.R
@@ -247,10 +247,12 @@ balloonplot.default <- function(x,y,z,
            )
 
       ## row totals
+      rowlabs <- format(c(sumz, colsumz), digits=label.digits)[-1]
+      width <- max(strwidth(rowlabs),na.rm=TRUE)
       text(
-           x=nx + nlabels.y*rowmar+0.25+strwidth("'"),
+           x=nx + nlabels.y*rowmar-0.25+width,
            y= (ny:1),
-           labels=format(c(sumz, colsumz), digits=label.digits)[-1],
+           labels=rowlabs,
            font=1,
            cex=par("cex")*0.75,
            adj=c(1.0,0.5)           
@@ -258,12 +260,12 @@ balloonplot.default <- function(x,y,z,
 
       ## overall total
       text(
-           x=nx + nlabels.y*rowmar,
+           x=nx + nlabels.y*rowmar-0.25+width,
            y=0.25,
            labels=sumz,
            font=1,
            cex=par("cex")*0.75,
-           adj=c(0.5,0.0)           
+           adj=c(1.0,0.0)           
            )
     }
      
