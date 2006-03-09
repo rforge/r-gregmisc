f5db5532a1e20a2c4de7daba2ed8f00d024f9974
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2006-03-09 14:16:45 +0000

    Automatic update of wiki information
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@924 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/OpenSourceInPharma/rwn-wiki/getwiki.sh
+++ trunk/OpenSourceInPharma/rwn-wiki/getwiki.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd /home/warnes/src/R/r-gregmisc/OpenSourceInPharma/rwn-wiki 
 wget -N -r -nd ftp://research.warnes.net:8021/statcomp/projects/CommercialR/
 svn add *
 svn commit -m "Automatic update of wiki information"
