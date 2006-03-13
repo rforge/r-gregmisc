7751361c7d7fee02852508b0c5d3ef254a745391
nj7w <nj7w@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2006-03-13 19:50:48 +0000

    Added trim.character and trim.factor as per Gregor's suggestions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@939 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gdata/R/trim.R
+++ trunk/gdata/R/trim.R
@@ -6,3 +6,15 @@ trim <- function(s)
     s <- sub(" +$","",s)
     s
   }
+
+trim.character <- function(s)
+{
+  return(trim(s))
+}
+
+trim.factor <- function(s)
+{
+  levels(s) <- trim(levels(s))
+  return(s)
+}
+
