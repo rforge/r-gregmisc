d621cb613968a69dbff050635e9569a4b0df0f2f
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2006-11-14 18:13:02 +0000

    Create 2.3.2 tag
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1009 45178186-c70b-4eec-b05e-9f6c5a094bf4

---
+++ tags/gdata-2.3.2/R/drop.levels.R
@@ -0,0 +1,24 @@
+
+drop.levels  <- function(x, reorder=TRUE, ...)
+  UseMethod("drop.levels", x=x)
+
+drop.levels.default <- function(x, reorder=TRUE, ...)
+  x
+
+drop.levels.factor <- function(x, reorder=TRUE, ...)
+{
+  x <- factor(x)
+  if(reorder) x <- reorder(x, ...)
+  x
+}
+
+drop.levels.list <- function(x, reorder=TRUE, ...)
+{
+  lapply(x, drop.levels, reorder=reorder, ...)
+}
+
+drop.levels.data.frame <- function(x, reorder=TRUE, ...)
+{
+  x[] <- drop.levels.list(x, reorder=reorder, ...)
+  x
+}
