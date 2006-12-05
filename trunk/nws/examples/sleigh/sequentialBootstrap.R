1e2cea7e1b7e1dacbe94c6c089b5d2b97d8b3d11
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2006-12-05 16:52:38 +0000

    Remove nws from repository
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1037 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/nws/examples/sleigh/sequentialBootstrap.R
+++
@@ -1,9 +0,0 @@
-# bootstrapping example taken from SNOW tutorial
-# This example can also be found in boot package's help menu
-
-source('nuclearBootstrapInit.R')
-
-timing<-system.time(nuke.boot<-boot(nuke.data, nuke.fun, R=20000, m=1, 
-		    fit.pred=new.fit, x.pred=new.data))[3]
-
-cat("bootstrap nuclear data: ", timing, "\n\n")
