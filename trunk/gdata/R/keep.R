ea3fc4e8b287aac7316239f99f4a0fcb300997c8
arnima <arnima@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2007-01-28 10:42:24 +0000

    Throw warnings rather than errors
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1042 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gdata/R/keep.R
+++ trunk/gdata/R/keep.R
@@ -1,14 +1,20 @@
 keep <- function(..., list=character(0), sure=FALSE)
 {
   if(missing(...) && missing(list))
-    stop("Keep something, or use rm(list=ls()) to clear workspace.")
+  {
+    warning("Keep something, or use rm(list=ls()) to clear workspace. ",
+            "Nothing was removed.")
+    return(invisible(NULL))
+  }
   names <- as.character(substitute(list(...)))[-1]
   list <- c(list, names)
   keep.elements <- match(list, ls(1))
-
   if(any(is.na(keep.elements)))
-    stop("You tried to keep \"", list[which(is.na(keep.elements))[1]],
-         "\" which doesn't exist in workspace. Nothing was removed.", sep="")
+  {
+    warning("You tried to keep \"", list[which(is.na(keep.elements))[1]],
+            "\" which doesn't exist in workspace. Nothing was removed.", sep="")
+    return(invisible(NULL))
+  }
 
   if(sure)
     rm(list=ls(1)[-keep.elements], pos=1)
