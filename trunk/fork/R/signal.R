4f93b1557360cebf8ab5df2feaebe20d78f13be7
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2007-02-05 23:17:07 +0000

    Handle arguments better
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1044 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/fork/R/signal.R
+++ trunk/fork/R/signal.R
@@ -1,27 +1,24 @@
 # $Id: kill.R 340 2004-05-25 19:12:32Z warnes $
 
-signal <- function(signal, action=c("ignore","restore") )
+signal <- function(signal, action=c("ignore","default") )
   {
-    action=match.args(action)
+    action=match.arg(action)
     
     if(is.character(signal))
-      sig=sigval(signal)$val
-    else (is.numeric(signal) && !is.na(signal))
-      sig=signal
+      sig <- sigval(signal)$val
+    else if(is.numeric(signal) && !is.na(signal))
+      sig <- signal
     else
       stop("Illegal value for signal")
     
     act <- switch(
                   action,
                   "ignore"=0,
-                  "restore"=1
+                  "default"=1
                   )
-
-    msg1 = 
-    
     .C(
        "Rfork_signal",
-       as.integer(signal),
+       as.integer(sig),
        as.integer(act),
        PACKAGE="fork"
        )
