b29082ed9ab475996ddc408911cefddad5cd2a60
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2007-03-01 22:38:28 +0000

    Changes in response to review, part 1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1059 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/PathwayModeling/thesispaper/R/plotConverged_nocolor.R
+++ trunk/PathwayModeling/thesispaper/R/plotConverged_nocolor.R
@@ -1,6 +1,10 @@
-`plotConverged_nocolor` <-
-function() {
+`plotConverged_nocolor` <- function()
+{
+    oldpar <- par(no.readonly=TRUE)
+    par(mar=c(4, 3, 0.75, 0.75) + 0.1)
+
     maxY <- 0.0012
+    
     for (i in 1:8) {
 	p <- paste("d",i,sep="")
 	plotDensity_nocolor(output12[,i],p,5000,Ylim=maxY,lineType=2)
@@ -14,5 +18,8 @@ function() {
     plotDensity_nocolor(output16[,9],'d9',5,Ylim=5.7,lineType=3)
     par(new=T)
     plotDensity_nocolor(output25[,9],'d9',5,Ylim=5.7,lineType=4)
-}
+
+    par(oldpar)
+
+  }
 
--- trunk/PathwayModeling/thesispaper/R/plotDensity_nocolor.R
+++ trunk/PathwayModeling/thesispaper/R/plotDensity_nocolor.R
@@ -1,12 +1,22 @@
-`plotDensity_nocolor` <-
-function(data,parameter="di",prior,Ylim=0,lineType=1) {
+`plotDensity_nocolor` <- function(data,
+                                  parameter="di",
+                                  prior,
+                                  Ylim=0,
+                                  lineType=1
+                                  )
+  {
+  
     dens<-density(data)
     deltaX<-(max(dens$x)-min(dens$x))/length(dens$x)
     sumY<-sum(dens$y)
     maxY<-max(dens$y, Ylim)
-    X<-seq(1,5*prior,0.05*prior)
-    plot(X,3*dchisq(3*X/prior,5)/prior,type='l',xlim=c(0,5*prior),ylim=c(0,maxY),ylab="density",xlab=parameter,lty=1)
-    par(new=T)
-    plot(dens$x,dens$y,xlim=c(0,5*prior),ylim=c(0,maxY),type='l',ylab="",xlab="",lty=lineType)
-}
+    X<-seq(1,5*prior, 0.05*prior)
+    
+    plot(X, 3*dchisq(3*X/prior, 5)/prior, type='l',
+         xlim=c(0, 5*prior), ylim=c(0, maxY),
+         ylab="density", xlab=parameter,
+         lty=1, lwd=2)
+
+    lines(dens$x, dens$y, lty=lineType, lwd=2)
+  }
 
