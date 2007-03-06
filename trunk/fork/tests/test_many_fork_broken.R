099cb87669fa15fa2cc151626fc042060c8fc712
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2007-03-06 22:47:07 +0000

    Create SysV and BSD tests for code to prevent zombie process creation, update code that intentionally creates zombies.  Move sub-process creation and zombie test code to a function for use by all three test files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1065 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/fork/tests/test_many_fork_broken.R
+++ trunk/fork/tests/test_many_fork_broken.R
@@ -4,33 +4,11 @@
 
 library(fork)
 
-cat("Generating 100 child processes (to become zombies)...\n")
-for(i in 1:100)
-  {
-    pid = fork(slave=NULL) 
-    if(pid==0)
-      { 
-        #cat("Hi from child process",getpid(),".\n");
-        #Sys.sleep(10);
-        #cat("Bye from child process",getpid(),".\n");
-        exit() 
-      }
-  }
+source("checkZombies.R")
 
-cat("Give them 10 seconds to die and exit..\n")
-Sys.sleep(10)
+nZombies <- checkZombies()
 
-cat("Check the process table to see if there are any zombies...\n")
-if(TRUE) # BSD-style PS command (Linux, Mac OSX, NetBSD)
-{
-  statusList = system("ps -o stat", intern=TRUE)[-1]
-} else
-{
-  statusList = system("ps -o s", intern=TRUE)[-1]
-}
-
-zombies = grep("[Zz]", statusList, value=TRUE)
-if(length(zombies)>50)
-  stop(length(zombies), " Zombie Processes Present!")
+if(nZombies)
+  stop("Zombie processes created")
 else
-  cat("Done. No Zombies present.\n")
+  cat("Success!\n")
