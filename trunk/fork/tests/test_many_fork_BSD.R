07b3b5e9063778828e9d97fb1f7909a03238e302
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2007-03-06 22:47:29 +0000

    Create SysV and BSD tests for code to prevent zombie process creation, update code that intentionally creates zombies.  Move sub-process creation and zombie test code to a function for use by all three test files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1066 45178186-c70b-4eec-b05e-9f6c5a094bf4

---
+++ trunk/fork/tests/test_many_fork_BSD.R
@@ -0,0 +1,22 @@
+## This script starts child processes, and sets up a 'dummy' SIGCLD
+## hander to handle child process notifications. Hopefully on both BSD
+## and Sys5 derived systems, this should cause child processes to exit
+## and die cleanly, IE without becoming zombies.
+
+library(fork)
+
+## start signal handler
+handleSIGCLD()
+
+source("checkZombies.R")
+
+nZombies <- checkZombies()
+
+if(nZombies) {
+  stop("Zombie processes created")
+} else {
+  cat("Success!\n")
+}
+
+# remove signal handler
+restoreSIGCLD()
