70a4462ddb1366e59bee10b61eeadb34bf586004
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2007-04-17 15:00:06 +0000

    Finalize revisions for IET Systems Biology
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1090 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/PathwayModeling/thesispaper/R/plotConverged_nocolor.R
+++ trunk/PathwayModeling/thesispaper/R/plotConverged_nocolor.R
@@ -7,19 +7,17 @@
     maxY <- 0.0012
     
 #    for (i in 1:8) {
-    for (i in c(1,3,8)) {    
+    for (i in c(1,4)) {    
 	p <- paste("d",i,sep="")
 	plotDensity_nocolor(output12[,i],p,5000,Ylim=maxY,lineType=2)
-	par(new=T)
-	plotDensity_nocolor(output16[,i],p,5000,Ylim=maxY,lineType=3)
-	par(new=T)
-	plotDensity_nocolor(output25[,i],p,5000,Ylim=maxY,lineType=4)
+	plotDensity_nocolor(output16[,i],p,5000,Ylim=maxY,lineType=3, add=TRUE)
+	plotDensity_nocolor(output25[,i],p,5000,Ylim=maxY,lineType=4, add=TRUE)
     }
-#    plotDensity_nocolor(output12[,9],'d9',5,Ylim=5.7,lineType=2)
-#    par(new=T)
-#    plotDensity_nocolor(output16[,9],'d9',5,Ylim=5.7,lineType=3)
-#    par(new=T)
-#    plotDensity_nocolor(output25[,9],'d9',5,Ylim=5.7,lineType=4)
+    # prior mode is 5, but fits are all much less than 5 so scale down so range
+    # is [0,5]
+    plotDensity_nocolor(output12[,9],'d9',5,Ylim=5.7,lineType=2, Xlim=5)
+    plotDensity_nocolor(output16[,9],'d9',5,Ylim=5.7,lineType=3, Xlim=5, add=TRUE)
+    plotDensity_nocolor(output25[,9],'d9',5,Ylim=5.7,lineType=4, Xlim=5, add=TRUE)
 
     par(oldpar)
 
--- trunk/PathwayModeling/thesispaper/R/plotDensity_nocolor.R
+++ trunk/PathwayModeling/thesispaper/R/plotDensity_nocolor.R
@@ -2,7 +2,9 @@
                                   parameter="di",
                                   prior,
                                   Ylim=0,
-                                  lineType=1
+                                  Xlim=5*prior,
+                                  lineType=1,
+                                  add=FALSE
                                   )
   {
   
@@ -10,12 +12,17 @@
     deltaX<-(max(dens$x)-min(dens$x))/length(dens$x)
     sumY<-sum(dens$y)
     maxY<-max(dens$y, Ylim)
-    X<-seq(1,5*prior, 0.05*prior)
-    
-    plot(X, 3*dchisq(3*X/prior, 5)/prior, type='l',
-         xlim=c(0, 5*prior), ylim=c(0, maxY),
-         ylab="density", xlab=parameter,
-         lty=1, lwd=2)
+    X <- seq(0,Xlim, 0.05*prior)    
+
+    if(!add)
+      plot(X,
+           3*dchisq(3*X/prior, 5)/prior,
+           type='l',
+           xlim=c(0,Xlim),
+           ylim=c(0, maxY),
+           ylab="density",
+           xlab=parameter,
+           lty=1, lwd=2)
 
     lines(dens$x, dens$y, lty=lineType, lwd=2)
   }
