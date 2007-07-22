08188a5781f3f0041462f96c71326de41c167e5d
arnima <arnima@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2007-07-22 09:27:18 +0000

    Reverted back to as.character(substitute(x)), so user can run keep(x), keep("x"), Args(x), and Args("x").
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1103 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gdata/R/keep.R
+++ trunk/gdata/R/keep.R
@@ -6,7 +6,7 @@ keep <- function(..., list=character(0), sure=FALSE)
             "Nothing was removed.")
     return(invisible(NULL))
   }
-  names <- deparse(substitute(list(...)))[-1]
+  names <- as.character(substitute(list(...)))[-1]
   list <- c(list, names)
   keep.elements <- match(list, ls(1))
   if(any(is.na(keep.elements)))
