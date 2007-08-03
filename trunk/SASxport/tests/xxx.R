8a764714d72bd76cdb59e748f8c6be3020ed52bf
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2007-08-03 00:35:07 +0000

    Add more tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1109 45178186-c70b-4eec-b05e-9f6c5a094bf4

---
+++ trunk/SASxport/tests/xxx.R
@@ -0,0 +1,54 @@
+library(SASxport)
+
+## manually create a data set
+abc <- data.frame( x=c(1, 2, NA, NA ), y=c('a', 'B', NA, '*' ) )
+
+## add a format specifier (not used by R)
+attr(abc$x, 'format') <- 'date7.'
+
+## add a variable label (not used by R)
+attr(abc$y, 'label')  <- 'character variable'
+
+# create a SAS XPORT file from our local data fram
+write.xport(abc,
+            filename="xxx2.xpt",
+            cDate=strptime("28JUL07:21:08:06 ", format="%d%b%y:%H:%M:%S"),
+            osType="SunOS",
+            sasVer="9.1"
+            )
+
+# read the original SAS data file
+abc.SAS <- read.xport("xxx.xpt")
+
+## read.xport currently doesn't store the format and label attributes...
+attr(abc.SAS$X, 'format') <- 'date7.'
+attr(abc.SAS$Y, 'label')  <- 'character variable'
+
+# create a SAS XPORT file from the SAS data
+write.xport(abc=abc.SAS,
+            filename="xxx3.xpt",
+            cDate=strptime("28JUL07:21:08:06 ", format="%d%b%y:%H:%M:%S"),
+            osType="SunOS",
+            sasVer="9.1"
+            )
+
+
+
+
+## Load both files back in as raw data
+a.1 <- readBin( con="xxx.xpt",  what=raw(), n=1e5 )
+a.2 <- readBin( con="xxx2.xpt", what=raw(), n=1e5 )
+a.3 <- readBin( con="xxx3.xpt", what=raw(), n=1e5 )
+
+## R doesn't have multiple NA types, while SAS does.  The original
+## file contains a SAS '.A' missing value, while what we've created
+## contains an ordinary '.' missing value, so mash this one byte to
+## avoid a comparison error for this know limitation.  
+
+a.1[1089] <- as.raw("0x2e")
+
+## Test that the files are otherwise identical
+assert( all(a.1 == a.2) )
+assert( all(a.1 == a.3) )
+
+
