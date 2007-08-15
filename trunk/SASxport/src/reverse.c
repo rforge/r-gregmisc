8a64b6da2edaefb7012236ce5936bb0ebb23122d
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2007-08-15 06:06:09 +0000

    Fix reverse.c because we need to swap everything to match Big-Endian, rather than Little-Endian.  Also, dont' call the macro.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1145 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/SASxport/src/reverse.c
+++ trunk/SASxport/src/reverse.c
@@ -1,4 +1,4 @@
-#include "writeSAS.h"
+//#include "writeSAS.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -6,30 +6,29 @@
 #import  <assert.h>
 #import  <sys/types.h>
 
-/* reverse: convert current byte order to little endian */
-void reverse( u_char *intp, size_t size)
+/* reverse: convert current byte order to big endian */
+void reverse( unsigned char *intp, size_t size)
 {
-  static u_char endianTest[2] =   {0x01,0x00};
+  static unsigned char endianTest[2] =   {0x01,0x00};
   size_t i;
-  u_char tmp;
+  unsigned char tmp;
 
 #if !defined(BIG_ENDIAN) && !defined(LITTLE_ENDIAN)
   /* Test if we are on a big endian or little endian platform */
-  if( (short) *endianTest == 1 ) 
+  if( (short) *endianTest != 1 ) 
     {  
-      /* Little Endian */
-      /* Do nothing    */
+      /* The native byte order is big endian, so do nothing */
+      sprintf("Big Endian Machine!\n");
       return;  
     }
 #endif
 
-  /* Big Endian */
-  /* Swap bytes */
+  /* If native byte order is little endian, we need to swap bytes */
   for(i=0; i < size/2; i++)
     {
-      tmp = (u_char) intp[i];
+      tmp = (unsigned char) intp[i];
       intp[i] = intp[size-i-1];
-      intp[size-i] = tmp;
+      intp[size-i-1] = tmp;
     }
   
   return;
@@ -39,41 +38,41 @@ void reverse( u_char *intp, size_t size)
 /* test code */
 void test_reverse()
 {
-  u_char  byte_pattern[1] = { 0x00 };
-  u_char  byte_value      = 0x00;
+  unsigned char  byte_pattern[1] = { 0x00 };
+  unsigned char  byte_value      = 0x00;
   
-  u_char  short_pattern[2] = { 0x00, 0x01 };  /* NB: little endian byte pattern */
-  short   short_value       = 0x0100;          /* NB: hex is written big endian */
+  unsigned char  short_pattern[2] = { 0x01, 0x00 };   /* NB: big endian byte pattern */
+  short   short_value       = 0x0100;          /* NB: hex is also written big endian */
   
-  u_char  int_pattern[4]   = { 0x0, 0x01, 0x02, 0x03 };
+  unsigned char  int_pattern[4]   = { 0x03, 0x02, 0x01, 0x00 };
   int     int_value        = 0x03020100; 
 
-  u_char  long_pattern[4]  = { 0x0, 0x01, 0x02, 0x03 };
+  unsigned char  long_pattern[4]  = { 0x03, 0x02, 0x01, 0x00 };
   long    long_value       = 0x03020100; 
 
   /* Do the reverse, then test */
 
   /* byte */
-  REVERSE( &byte_value, sizeof(u_char) );
-  assert( (u_char) *byte_pattern == byte_value );
+  reverse( &byte_value, sizeof(unsigned char) );
+  assert( (unsigned char) *byte_pattern == byte_value );
 
   /* short */
-  REVERSE( &short_value, sizeof(short) );
+  reverse( (unsigned char*) &short_value, sizeof(short) );
   assert( *((short *) short_pattern) == short_value );
 
   /* int */
-  REVERSE( &int_value, sizeof(int) );
+  reverse( (unsigned char*) &int_value, sizeof(int) );
   assert( *((int *) int_pattern) == int_value );
 
   /* long */
-  REVERSE( &long_value, sizeof(long) );
+  reverse( (unsigned char*) &long_value, sizeof(long) );
   assert( *((long*) long_pattern) == long_value );
 
 }
 
 
 #ifdef DO_TEST
-int main(int argc, u_char *argv)
+int main(int argc, char *argv)
 {
   test_reverse();
 }
