624ad5e066512e70a608b77264690461f6079ff9
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2007-09-25 19:53:42 +0000

    Display version and support information on package load
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1189 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/SASxport/R/zzz.R
+++ trunk/SASxport/R/zzz.R
@@ -1,4 +1,22 @@
-.First.lib <- function(lib, pkg) {
-  library.dynam("SASxport", pkg, lib)
+loadMessage <- function()
+{
+  cat("\n")
+  ver <-packageDescription("SASxport", fields="Version") 
+  date <- packageDescription("SASxport", fields="Date") 
+  cat("Loaded SASxport version ", ver,  " (", date ,").\n", sep="")
+  cat("\n")
+  cat("  Updates and technical support available from Random Technologies, LLC\n")
+  cat("  <http://random-technologies-llc.com/contact>\n")
+  cat("\n")
 }
 
+
+
+.First.lib <- function(lib, pkg) {
+  library.dynam("SASxport", pkg, lib)
+  loadMessage()
+}
+
+.onLoad <- function(lib, pkg) {
+  loadMessage()
+}
