d850d951284ca54d9328b4a95fe93787b51323b3
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2010-01-23 02:30:09 +0000

    Add perl scripts to extract worksheet names and sheet count from Excel files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1377 45178186-c70b-4eec-b05e-9f6c5a094bf4

---
+++ trunk/gdata/inst/perl/sheetNames.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+BEGIN  {
+use File::Basename;
+unshift(@INC, dirname $0);
+}
+
+use strict;
+use Spreadsheet::ParseExcel;
+
+# declare some varibles local
+my($row, $col, $sheet, $cell, $usage, $basename, $sheetnumber, $filename);
+
+##
+## Usage information
+##
+$usage = <<EOF;
+
+sheetCount.pl <excel file>
+
+Output is the number of sheets in the excel file.
+
+EOF
+
+##
+## parse arguments
+##
+
+if(!defined($ARGV[0]))
+  {
+    print $usage;
+    exit 1;
+  }
+
+my $fileName=$ARGV[0];
+
+##
+## open spreadsheet
+##
+
+my $oExcel = new Spreadsheet::ParseExcel;
+
+open(FH, "<$fileName") or die "Unable to open file '$fileName'.\n";
+close(FH);
+
+my $oBook = $oExcel->Parse($fileName);
+
+## Get list all worksheets in the file
+my @sheetlist =  (@{$oBook->{Worksheet}});
+
+foreach my $sheet (@sheetlist)
+{
+  print "\"$sheet->{Name}\" ";
+}
+
+print "\n";
+
