044621bcd1162f07e3eb43d072ef92a16bce283c
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2010-01-28 16:00:12 +0000

    More work on Win32 building
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1416 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gdata/src/build.bat
+++ trunk/gdata/src/build.bat
@@ -1,6 +1,16 @@
+
 set TARGET=Compress-Raw-Zlib-2.024
 set SHELL=
+
+echo ** Bulding Perl library %TARGET% ..
 cd %TARGET%
 perl Makefile.PL PREFIX=../../inst/perl LIB=../../inst/perl 
 dmake
 dmake install
+
+echo ** Replace Unix symlinks with copies ..
+cd ../../inst/perl
+copy xls2csv.pl xls2tab.pl
+copy xls2csv.pl xls2tsv.pl
+copy sheetCount.pl sheetNames.pl
+
