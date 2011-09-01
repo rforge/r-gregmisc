145a7efd18f533b7d182b6bdf6216fed4eba52c5
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2011-09-01 19:37:38 +0000

    Improvements to ci2d():
    - Add option to utilize KernDensity::bkde2D to estimate the 2-d
      density (now the default).
    
    - Add option to display points for original data on generated plots
    
    - Name elements of returned contour list according to the significance
      level to make it easier to select desired contours.
    
    - Improve handling of x and y labels
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1486 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gplots/R/ci2d.R
+++ trunk/gplots/R/ci2d.R
@@ -1,3 +1,5 @@
+## $Id$
+
 ## first(...) selects the first element of which(...)
 first <- function(x,...)
   {
@@ -19,23 +21,77 @@ last <- function(x,...)
 }
 
 ## non-parametric 2 dimensional approximate confidence interval
-## based on 2 dimensional histogram
 ci2d <- function(x,
                  y = NULL,
-                 nbins=25,
+                 nbins=51,
+                 method=c("bkde2D","hist2d"),
+                 bandwidth,
+                 factor=1.0,
+                 
                  ci.levels=c(0.50,0.75,0.90,0.95,0.975),
+                 
                  show=c("filled.contour","contour","image","none"),
-                 xlab=deparse(substitute(x)),
-                 ylab=deparse(substitute(y)),
                  col=topo.colors(length(breaks)-1),
+                 show.points=FALSE,
+                 pch=par("pch"),
+                 points.col="red",
+                 xlab, ylab, 
                  ...)
   {
 
-    show=match.arg(show)
+    show <- match.arg(show)
+    method <- match.arg(method)
     breaks <- unique(c(0, ci.levels, 1.0))
-    
-    h2d <- hist2d(x,y, show=FALSE, nbins=nbins, ...)
-    h2d$density <- h2d$counts / sum(h2d$counts, na.rm=TRUE)
+
+    # get labels for x and y
+    if (missing(xlab)) 
+      xlab <- if (missing(x)) 
+        ""
+      else deparse(substitute(x))
+    if (missing(ylab)) 
+      ylab <- if (missing(y)) 
+        ""
+      else deparse(substitute(y))
+
+    if(!is.null(y))
+      x <- cbind(x,y)
+
+    if(method=="hist2d")
+      {
+        h2d <- hist2d(x,
+                      show=FALSE,
+                      nbins=nbins,
+                      ...)
+        ## normalize
+        h2d$density <- h2d$counts / sum(h2d$counts, na.rm=TRUE)
+      }
+    else if (method=="bkde2D")
+      {
+        if(length(nbins)==1)
+          nbins <- c(nbins, nbins)
+
+        if(missing(bandwidth))
+          {
+            h.x = dpik(x[,1])
+            h.y = dpik(x[,2])
+            bandwidth <-  c(h.x, h.y)
+          }
+
+        est <- bkde2D(x,
+                      bandwidth=bandwidth*factor,
+                      gridsize=nbins,
+                      ...
+                      )
+
+        h2d <- list()
+        h2d$x <- est$x1
+        h2d$y <- est$x2
+        h2d$counts <- est$fhat
+        h2d$density <- est$fhat / sum(est$fhat) # normalize
+
+      }
+    else
+      stop("Unknown method: '", method, "'")
 
     uniqueVals <- rev(unique(sort(h2d$density)))
     cumProbs <- sapply(uniqueVals,
@@ -49,14 +105,26 @@ ci2d <- function(x,
         image(h2d$x, h2d$y, h2d$cumDensity,
               xlab=xlab, ylab=ylab,
               breaks=breaks, col=col)
+        if(show.points)
+            points(x[,1], x[,2], pch=pch, col=points.col);          
       }
     else if(show=="filled.contour")
       {
+        if(show.points)
+          plot.title <- function() {
+            points(x[,1], x[,2], pch=pch, col=points.col);
+          }
+        else
+          plot.title <- function() {}
+
+        
         filled.contour(h2d$x, h2d$y, h2d$cumDensity,
                        levels=breaks,
                        col=col,
-                       key.axes={ axis(4, at=breaks);
-                                  title("\nCI Level") }
+                       xlab=xlab,
+                       ylab=ylab,
+                       plot.title=plot.title(),
+                       key.title=title("\nCI Level")
                        )
        }
     else if(show=="contour")
@@ -65,14 +133,26 @@ ci2d <- function(x,
         contour(h2d$x, h2d$y, h2d$cumDensity,
                 levels=tmpBreaks,
                 labels=tmpBreaks,
+                xlab=xlab,
+                ylab=ylab,
                 nlevels=length(tmpBreaks),
                 col=col
                 )
+        if(show.points)
+            points(x[,1], x[,2], pch=pch, col=points.col);          
       }
-    
 
     h2d$contours <- contourLines(h2d$x, h2d$y, h2d$cumDensity,
                                  levels=breaks, nlevels=length(breaks))
-    names
+
+    # use the confidence level value as the name in the contour list
+    names(h2d$contours) <- sapply(h2d$contours, function(x) x$level)
+
+    # convert each contour into a (x,y) dataframe 
+    h2d$contours <- lapply( h2d$contours,
+                            function(J) data.frame(x=J$x, y=J$y) )
+
+    h2d$call <- match.call()
+    
     invisible(h2d)
   }
