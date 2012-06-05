b00ce0ffa6b3f958a1ed882e89d466d4b966bec6
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2012-06-05 19:07:53 +0000

    Fix error in ls.funs() that occurs when there are no objects in the environment.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1536 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gdata/R/ls.funs.R
+++ trunk/gdata/R/ls.funs.R
@@ -3,7 +3,12 @@ ls.funs <- function (...)
     mycall <- match.call()
     mycall[[1]] <- as.name("ls")
     nameList <- eval.parent(mycall)
-    funcFlags <- sapply( nameList, function(x) is.function(get(x)) )
-    nameList[funcFlags]
+    if(length(nameList)>0)
+      {
+        funcFlags <- sapply( nameList, function(x) is.function(get(x)) )
+        return(nameList[funcFlags])
+      }
+    else
+      return( list() )
   }
                         
