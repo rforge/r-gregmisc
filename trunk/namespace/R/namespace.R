aa39163124b9b54b345e1dfa5a53884a53d8ce19
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2012-09-19 16:57:08 +0000

    Move makeNamespace function into a separate file and include the original copyright notice.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1612 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/namespace/R/namespace.R
+++ trunk/namespace/R/namespace.R
@@ -56,30 +56,3 @@ if(exists(".getNamespace", where="package:base", mode="function")) {
       return(getNamespace(name))
   }
 }
-
-## Copy of function defined inside of base::loadNamespace
-makeNamespace <- function(name, version = NULL, lib = NULL) {
-    impenv <- new.env(parent = .BaseNamespaceEnv, hash = TRUE)
-    attr(impenv, "name") <- paste("imports", name, sep=":")
-    env <- new.env(parent = impenv, hash = TRUE)
-    name <- as.character(as.name(name))
-    version <- as.character(version)
-    info <- new.env(hash = TRUE, parent = baseenv())
-    assign(".__NAMESPACE__.", info, envir = env)
-    assign("spec", c(name = name,version = version), envir = info)
-    setNamespaceInfo(env, "exports", new.env(hash = TRUE, parent = baseenv()))
-    dimpenv <- new.env(parent = baseenv(), hash = TRUE)
-    attr(dimpenv, "name") <- paste("lazydata", name, sep=":")
-    setNamespaceInfo(env, "lazydata", dimpenv)
-    setNamespaceInfo(env, "imports", list("base" = TRUE))
-    ## this should be an absolute path
-    setNamespaceInfo(env, "path",
-                     normalizePath(file.path(lib, name), "/", TRUE))
-    setNamespaceInfo(env, "dynlibs", NULL)
-    setNamespaceInfo(env, "S3methods", matrix(NA_character_, 0L, 3L))
-    assign(".__S3MethodsTable__.",
-           new.env(hash = TRUE, parent = baseenv()),
-           envir = env)
-    registerNamespace(name, env)
-    env
-}
