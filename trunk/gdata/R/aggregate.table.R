03fc9ff8de8ab41cb80fcef255724392b0eb0007
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2013-06-28 20:26:56 +0000

    Improve deprecated message
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1681 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gdata/R/aggregate.table.R
+++ trunk/gdata/R/aggregate.table.R
@@ -1,8 +1,8 @@
 # $Id$
 
-aggregate.table <- function(x, by1, by2, FUN=mean, ... )
+aggregate.table <- function(x, by1, by2, FUN=mean, ...)
   {
-    warning("'aggregate.table' is depreciated.",
+    warning("'aggregate.table' is deprecated and will be removed in a future version of the gdata package. ",
             "Please use 'tapply(X=",
             deparse(substitute(x)),
             ", INDEX=list(",
