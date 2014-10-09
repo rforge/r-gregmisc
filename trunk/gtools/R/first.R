36e77e2cc2cb26dc09183883b2f8d38c4fcdbf72
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2014-10-09 18:52:58 +0000

    Make right() and left() S3 methods for classes data.frame and matrix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1896 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gtools/R/first.R
+++ trunk/gtools/R/first.R
@@ -1,21 +1,26 @@
 first <- function(x) UseMethod("first")
 last  <- function(x) UseMethod("last")
+left  <- function(x, n) UseMethod("left")
+right <- function(x, n) UseMethod("left")
 
 first.default <- function(x) x[1]
-last.default  <- function(x) x[length(x)]
+last.default  <- function(x) x[length(x)] 
 
-first.list <- function(x) x[[1]]
-last.list  <- function(x) x[[length(x)]]
+first.list <- function(x, ...) x[[1]]
+last.list  <- function(x, ...) x[[length(x)]]
 
-left <- function(x, n=6)
+
+left.data.frame <- function(x, n=6)
 {
    n <- min(n, ncol(x))
    x[, 1:n]
 }
+left.matrix <- left.data.frame
 
-right <- function(x, n=6)
+right.data.frame <- function(x, n=6)
 {
    n <- min(n, ncol(x))
    x[, (ncol(x)-n+1):ncol(x)]
 }
+right.matrix <- right.data.frame
 
