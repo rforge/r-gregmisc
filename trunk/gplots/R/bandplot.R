424e8cb541efa43311ee0ce3ca077b02ac27cf66
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2014-12-01 22:52:47 +0000

    Convert bandplot to S3-method dispatch and add method for class formula.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1899 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gplots/R/bandplot.R
+++ trunk/gplots/R/bandplot.R
@@ -1,6 +1,9 @@
 # $Id$
 
-bandplot  <-  function(x,y,
+bandplot <- function(x, ...)
+    UseMethod("bandplot")
+
+bandplot.default  <-  function(x, y,
                        ...,
                        add=FALSE,
                        sd=c(-2:2),
@@ -53,3 +56,70 @@ bandplot  <-  function(x,y,
       }
     
   }
+
+
+bandplot.formula <- function(x,
+                             data=parent.frame(),
+                             subset,
+                             na.action,
+                             xlab=NULL,
+                             ylab=NULL,
+                             ...,
+                             add=FALSE,
+                             sd=c(-2:2),
+                             sd.col=c("magenta","blue","red",
+                                      "blue","magenta"),
+                             sd.lwd=c(2,2,3,2,2),
+                             sd.lty=c(2,1,1,1,2),
+                             method="frac", width=1/5,
+                             n=50
+                             )
+    {
+        if (missing(x) || (length(x) != 3))
+            stop("x missing or incorrect")
+        if (missing(na.action))
+            na.action <- getOption("na.action")
+        m <- match.call(expand.dots = FALSE)
+        if (is.matrix(eval(m$data, parent.frame())))
+            m$data <- as.data.frame(data)
+        m$formula <- m$x
+        m$...  <- m$x <- m$f <- m$iter <- m$delta <- NULL
+        m$xlab <- m$ylab <- m$add  <- m$sd <- NULL
+        m$sd.col <- m$sd.lwd <-  m$sd.lty <- NULL
+        m$method <- m$width <- m$n <- NULL
+
+        m[[1]] <- as.name("model.frame")
+        mf <- eval(m, parent.frame())
+        response <- attr(attr(mf, "terms"), "response")
+        
+        sx <- substitute(x)
+
+        if (is.null(xlab)) {
+            if (mode(x) != "name") 
+                xlab <- deparse(sx[[3L]])
+            else
+                xlab <- "x" 
+        }
+
+        if (is.null(ylab)) {
+            if (mode(x) != "name") 
+                ylab <- deparse(sx[[2L]])
+            else
+                ylab <- "y" 
+        }
+
+        bandplot.default(x=mf[[-response]],
+                 y=mf[[response]],
+                 ...,
+                 xlab=xlab,
+                 ylab=ylab,
+                 add=add,
+                 sd=sd,
+                 sd.col=sd.col,
+                 sd.lwd=sd.lwd,
+                 sd.lty=sd.lty,
+                 method=method,
+                 width=width,
+                 n=n
+                 )
+    }
