0031eca31fa2678524264d8fd68c4a9c66d2666d
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2015-04-25 05:54:28 +0000

    Complete work on object.size(), object_sizes methods, and humanReadable.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1957 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gdata/R/humanReadable.R
+++ trunk/gdata/R/humanReadable.R
@@ -1,61 +1,89 @@
-humanReadable <- function(x, standard=c("SI", "IEC"), units, digits=1, width=3, sep=" ")
+humanReadable <- function(x,
+                          units="auto",
+                          standard=c("IEC", "SI", "Unix"),
+                          digits=1,
+                          width=NULL,
+                          sep=" ",
+                          justify = c("right", "left"),
+                          ...
+                          )
 {
   ## --- Setup ---
 
-  suffix.decimal <- c("B", "kB",  "MB",  "GB",  "TB",  "PB",  "EB",  "ZB",  "YB")
-  suffix.binary  <- c("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
+  suffix.SI   <- c("B",  "kB",  "MB",  "GB",  "TB",  "PB",  "EB",  "ZB",  "YB")
+  suffix.IEC  <- c("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
+  suffix.Unix <- c("B" ,  "K",   "M",   "G",   "T",   "P",   "E",   "Z",   "Y")
 
   standard <- match.arg(standard)
-  
+  if(length(justify)==1) justfy <- c(justify, justify)
+
   ## --- Functions ---
 
   .applyHuman <- function(x, base, suffix, digits, width, sep)
   {
     ## Which suffix should we use?
     n <- length(suffix)
-    for(i in 1:n) {
-      if(x >= base) {
-        if(i < n) x <- x / base
-      } else {
-        break
-      }
-    }
+    i <- pmax(pmin(floor(log(x, base)), n-1),0)
+    if(!is.finite(i)) i <- 0
+    x <- x / base^i
     ## Formatting
-    if(is.null(width)) { ## the same formatting for all
-      x <- format(round(x=x, digits=digits), nsmall=digits)
-    } else {             ## similar to ls, du, and df
-      lenX <- nchar(x)
-      if(lenX > width) {
-        digitsMy <- width - (lenX - (lenX - (nchar(round(x)) + 1)))
-        digits <- ifelse(digitsMy > digits, digits, digitsMy)
-      }
-      if(i == 1) digits <- 0
-      x <- round(x, digits=digits)
-    }
-    paste(x, suffix[i], sep=sep)
+    if(is.null(width))
+        ## the same formatting for all
+        x <- format(round(x=x, digits=digits), nsmall=digits)
+    else
+        {
+            ## similar to ls, du, and df
+            lenX <- nchar(x)
+            if(lenX > width) {
+                digits <- pmax( width - nchar(round(x)) - 1, 0)
+            }
+            if(i == 0) digits <- 0
+            x <- round(x, digits=digits)
+        }
+    c(x, suffix[i+1])
   }
 
   ## -- Work
-  
-  if(any(x < 0)) stop("'x' must be positive")
-  if(standard == "SI") {
-    suffix <- suffix.decimal
-    base <- 10^3
-  } else {
-    suffix <- suffix.binary
-    base <- 2^10
-  }
 
-  if(!missing(units))
+  if(any(x < 0)) stop("'x' must be positive")
+  if(standard == "SI")
       {
-          units <- match.arg( units, suffix )
+          suffix <- suffix.SI
+          base <- 10^3
+      }
+  else if (standard=="IEC")
+      {
+          suffix <- suffix.IEC
+          base <- 2^10
+      }
+  else # (standard=="Unix)
+      {
+          suffix <- suffix.Unix
+          base <- 2^10
+      }
+
+  if(!missing(units) && units=="bytes")
+      {
+          retval <- rbind(x, "bytes")
+      }
+  else if(!missing(units) && units!="auto")
+      {
+          units <- suffix[match( toupper(units), toupper(suffix) )]
           power <- match(units, suffix ) -1
           X <- x/(base^power)
-          X <- format.default(round(x=X, digits=digits), nsmall=digits)
-          X <- paste(X, units)
-          X
+          X <- format.default(x=X, digits=digits, nsmall=digits)
+          retval <- rbind(X, rep(units, length(X)))
       }
   else
-      sapply(X=x, FUN=".applyHuman", base=base, suffix=suffix, digits=digits,
-             width=width, sep=sep)
+      retval <- sapply(X=x, FUN=".applyHuman", base=base, suffix=suffix,
+                       digits=digits, width=width, sep=sep)
+
+  if(all(justify == "none"))
+      paste(trim(retval[1,]), trim(retval[2,]), sep=sep)
+  else
+      paste(format(trim(retval[1,]), justify=justify[1]),
+            format(trim(retval[2,]), justify=justify[2]),
+            sep=sep)
+
 }
+
--- trunk/gdata/R/object.size.R
+++ trunk/gdata/R/object.size.R
@@ -1,38 +1,79 @@
-### object.size.R
 ###------------------------------------------------------------------------
 ### What: Print object size in human readable format - code
-### $Id$
-### Time-stamp: <2008-12-30 08:05:43 ggorjan>
 ###------------------------------------------------------------------------
-object.size <- function(...) 
+object.size <- function(...)
 {
   structure(sapply(list(...),
                    utils::object.size),
                    class=c("object_sizes", "numeric"))
 }
 
-print.object_sizes <- function(x, quote=FALSE, units,
-                                  humanReadable, ...)
-{  
-  xOrig <- x
-  if(missing(humanReadable)) {
-    opt <- getOption("humanReadable")
-    humanReadable <- ifelse(!is.null(opt), opt, FALSE)
-  }
-  if(humanReadable) {
-    print(humanReadable(x), quote=quote, ...)
-  } else {
-    class(x) <- "numeric"
-    NextMethod()
-  }
-  invisible(xOrig)
+print.object_sizes <- function(x,
+                               quote=FALSE,
+                               humanReadable=getOption("humanReadable"),
+                               standard="IEC",
+                               units,
+                               digits=1,
+                               width=NULL,
+                               sep=" ",
+                               ...)
+{
+    print(format(x,
+                 humanReadable=humanReadable,
+                 standard=standard,
+                 units=units,
+                 digits=digits,
+                 width=width,
+                 sep=sep),
+          quote=quote,
+          ...)
+
+
+    invisible(x)
 }
 
+format.object_sizes <- function(x,
+                                humanReadable=getOption("humanReadable"),
+                                standard="IEC",
+                                units,
+                                digits=1,
+                                width=NULL,
+                                sep=" ",
+                                ...)
+{
+    if( !missing(units) )
+       {
+           if (units=="bytes")
+               paste(x, "bytes")
+           else
+               humanReadable(x,
+                             standard=standard,
+                             units=units,
+                             digits=digits,
+                             width=width,
+                             sep=sep
+                             )
+       }
+    else if( is.null(humanReadable) || humanReadable==FALSE )
+        paste(x, "bytes")
+    else
+        humanReadable(x,
+                      standard=standard,
+                      units=units,
+                      digits=digits,
+                      width=width,
+                      sep=sep)
+
+}
+
+
+
 is.object_sizes <- function(x) inherits(x, what="object_sizes")
-   
+
 as.object_sizes <- function(x)
 {
-  if(!is.numeric(x)) stop("'x' must be numeric/integer")
+  if(!is.numeric(x) || any(x<0)) stop("'x' must be a positive numeric vector")
+
   class(x) <- c("object_sizes", "numeric")
   x
 }
@@ -43,67 +84,6 @@ c.object_sizes <- function(..., recursive=FALSE)
   if(is.numeric(x)) class(x) <- c("object_sizes", "numeric")
   x
 }
-
-humanReadable <- function(x, standard="SI", units, digits=1, width=3, sep=" ")
-{
-  ## --- Setup ---
-
-  suffix.decimal <- c("B", "kB",  "MB",  "GB",  "TB",  "PB",  "EB",  "ZB",  "YB")
-  suffix.binary  <- c("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB")
-    
-  ## --- Functions ---
-
-  .applyHuman <- function(x, base, suffix, digits, width, sep)
-  {
-    ## Which suffix should we use?
-    n <- length(suffix)
-    for(i in 1:n) {
-      if(x >= base) {
-        if(i < n) x <- x / base
-      } else {
-        break
-      }
-    }
-    ## Formatting
-    if(is.null(width)) { ## the same formatting for all
-      x <- format(round(x=x, digits=digits), nsmall=digits)
-    } else {             ## similar to ls, du, and df
-      lenX <- nchar(x)
-      if(lenX > width) {
-        digitsMy <- width - (lenX - (lenX - (nchar(round(x)) + 1)))
-        digits <- ifelse(digitsMy > digits, digits, digitsMy)
-      }
-      if(i == 1) digits <- 0
-      x <- round(x, digits=digits)
-    }
-    paste(x, suffix[i], sep=sep)
-  }
-
-  ## -- Work
-  
-  if(any(x < 0)) stop("'x' must be positive")
-  if(standard == "SI") {
-    suffix <- suffix.decimal
-    base <- 10^3
-  } else {
-    suffix <- suffix.binary
-    base <- 2^10
-  }
-
-  if(!missing(units))
-      {
-          units <- match.arg( units, suffix )
-          power <- which( units %in% suffix ) -1
-          X <- x/(base^power)
-          X <- format.default(round(x=x, digits=digits), nsmall=digits)
-          X <- paste(X, units)
-          X
-      }
-  else
-      sapply(X=x, FUN=".applyHuman", base=base, suffix=suffix, digits=digits,
-             width=width, sep=sep)
-}
-
 
 ###------------------------------------------------------------------------
 ### object.size.R ends here
