0c028ec073b01a5a76f7852c70ca113bfa568436
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2015-04-28 04:44:08 +0000

    - first() and last() are now simply wrappers to utils::head() and   utils::tail() with a default 'n=1' instead of 'n=6'. - Move code for left() and right() into a separate file.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@1978 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- trunk/gdata/R/first.R
+++ trunk/gdata/R/first.R
@@ -1,30 +1,3 @@
-first <- function(x) UseMethod("first")
-last  <- function(x) UseMethod("last")
-
-left  <- function(x, n=6) UseMethod("left")
-right <- function(x, n=6) UseMethod("left")
-
-
-first.default <- function(x) x[1]
-last.default  <- function(x) x[length(x)] 
-
-
-first.list <- function(x) x[[1]]
-last.list  <- function(x) x[[length(x)]]
-
-
-left.data.frame <- function(x, n=6)
-{
-   n <- min(n, ncol(x))
-   x[, 1:n]
-}
-left.matrix <- left.data.frame
-
-
-right.data.frame <- function(x, n=6)
-{
-   n <- min(n, ncol(x))
-   x[, (ncol(x)-n+1):ncol(x)]
-}
-right.matrix <- right.data.frame
-
+# Simply call 'first' or 'last' with a different default value for 'n'.
+first <- function(x, n=1, ...) head(x, n=n, ...)
+last  <- function(x, n=1, ...) tail(x, n=n, ...)
