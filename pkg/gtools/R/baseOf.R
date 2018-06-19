623a8bac5a75d141b1635edce54338ce63f09316
warnes <warnes@45178186-c70b-4eec-b05e-9f6c5a094bf4>
2018-06-19 16:43:05 +0000

    Improvements to baseOf() function.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r-gregmisc@2173 45178186-c70b-4eec-b05e-9f6c5a094bf4

--- pkg/gtools/R/baseOf.R
+++ pkg/gtools/R/baseOf.R
@@ -1,32 +1,73 @@
+# Transform integer to array of digits in specified
+baseOf <- function(v,
+                   b=10,
+                   l=1)
+{
+  if (is.null(v))
+    stop("v is null")
+  if(length(v)==0)
+    return(integer(0))
 
-# transform base
-#
-# This function rewrites regular integer numbers as an array of its digits.
-# The base of the numbering scheme may be changed away from 10,
-# which defines our decimal system, to any other integer value. For
-# b=2, the number is returned in the dual system. The least significant
-# digit has the highest index in the array, i.e. it appears on the right.
-#
-#   v = value of base 10 to be transformed
-#   b = new base
-#   l = minimal length of returned array (default is 1)
-# return value: array of factors, highest exponent first
-baseOf<-function(v,b=10,l=1) {
-	remainder<-v
-	i<-l
-	ret<-NULL
-	while(remainder>0 || i>0) {
-		#print(paste("i=",i," remainder=",remainder))
-		m<-remainder%%b
-		if (is.null(ret)) {
-			ret<-m
-		}
-		else {
-			ret<-c(m,ret)
-		}
-		remainder <- remainder %/% b
-		i<-i-1
-	}
-	return(ret)
+  if(any(as.integer(v) != v))
+    stop("non-integer value(s) provided for v.")
+
+  if (length(v) > 1)
+  {
+    # this returns a list which may have vectors of varying lenths
+    val.list <- lapply(X=v, FUN=baseOf.inner, b=b, l=l)
+    longest <- max(sapply(val.list, length))
+
+    # call again, forcing all elements to have the same lenth
+    retval  <- t(sapply(X=v, FUN=baseOf.inner, b=b, l=longest))
+
+    # add informative row and column names
+    rownames(retval) <- paste0('v.', v)
+    colnames(retval) <- paste0('b.', c(0, b^(1: (longest- 1) ) ) )
+
+    retval
+  }
+  else
+    retval <- baseOf.inner(v=v, b=b, l=l)
+
+  retval
 }
-                                   
+
+
+# Transform integer to array of digits in specified
+baseOf.inner <- function(v,
+                         b=10,
+                         l=1)
+{
+  if (is.na(v))
+    return(rep(NA, l))
+
+  if(v==0)
+    return(rep(0, l))
+
+  remainder <- v
+  i <- l
+  ret <- NULL
+  while(remainder > 0 || i >0)
+  {
+    #print(paste("i=",i," remainder=",remainder))
+    m <- remainder%%b
+    if (is.null(ret))
+    {
+      ret <- m
+    }
+    else
+    {
+      ret <- c(m,ret)
+    }
+    remainder  <-  remainder %/% b
+    i <- i-1
+  }
+
+  if(length(ret)>1)
+    names(ret) <-  c(0, b^( 1:(length(ret)- 1 ) ) )
+
+  return(ret)
+}
+
+
+
